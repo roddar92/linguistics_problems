831623d3f7e1533de0e394e118de0c41a1b69423
roddar92 <rodionowadarja@yandex.ru>
2016-06-19 00:22:43 +0300

    Syllables Module --- Reformat file

--- src/Syllables.py
+++ src/Syllables.py
@@ -1,29 +1,29 @@
 # -*- coding: utf-8 -*-
 class SyllableModule(object):
-	@staticmethod
+    @staticmethod
     def is_russian_vowel(symbol):
         return symbol in "аеиоуыэя"
-        
+
     @staticmethod
     def is_english_vowel(symbol):
         return symbol in "aeiou"
-        
+
     def syllables_count(self, word, engish=False):
-    	if engish and word[-1] == "e":
-    		leng = len(word) - 1
-    	else:
-    		leng = len(word)
-    		
-    	cnt = 0
-    	prev = ""
-    	for i in range(len):
-    		if english:
-	    		if (prev == "" or not self.is_english_vowel(prev)) and self.is_english_vowel(word[i])):
-	    			cnt += 1
-	    		prev = word[i]
-	    	else:
-	    		if (prev == "" or not self.is_russian_vowel(prev)) and self.is_russian_vowel(word[i])):
-	    			cnt += 1
-	    		prev = word[i]
-    	return cnt
-	
+        if engish and word[-1] == "e":
+            leng = len(word) - 1
+        else:
+            leng = len(word)
+
+        cnt = 0
+        prev = ""
+        for i in range(leng):
+            if engish:
+                if (prev == "" or not self.is_english_vowel(prev)) and self.is_english_vowel(word[i]):
+                    cnt += 1
+                prev = word[i]
+            else:
+                if (prev == "" or not self.is_russian_vowel(prev)) and self.is_russian_vowel(word[i]):
+                    cnt += 1
+                prev = word[i]
+
+        return cnt
