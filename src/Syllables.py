421451e638bee2f25e28568cdf8f497ff1039a46
roddar92 <rodionowadarja@yandex.ru>
2016-06-19 21:39:27 +0300

    SyllableModule --- split count of syllables by languages

--- src/Syllables.py
+++ src/Syllables.py
@@ -2,27 +2,38 @@
 class SyllableModule(object):
     @staticmethod
     def is_russian_vowel(symbol):
-        return symbol in "аеиоуыэя"
+        return symbol in "аеиоуыэюя"
+
+    @staticmethod
+    def is_russian_consonant(symbol):
+        return symbol in "бвгджзйклмнпрстфчцчшщ"
 
     @staticmethod
     def is_english_vowel(symbol):
-        return symbol in "aeiou"
+        return symbol in "aeiouy"
 
-    def syllables_count(self, word, engish=False):
-        if engish and word[-1] == "e":
-            leng = len(word) - 1
-        else:
-            leng = len(word)
+    @staticmethod
+    def is_english_consonant(symbol):
+        return symbol in "bcdfghjklmnpqrstvwxz"
+
+    def russian_syllables_count(self, word):
+        cnt = 0
+        for letter in word:
+            if self.is_russian_vowel(letter):
+                cnt += 1
+
+        return cnt
+
+    def english_syllables_count(self, word):
+        leng = len(word)
+        if word[-1] == "e":
+            leng -= 1
 
         cnt = 0
         prev = ""
         for i in range(leng):
-            if engish:
-                if (prev == "" or not self.is_english_vowel(prev)) and self.is_english_vowel(word[i]):
-                    cnt += 1
-                prev = word[i]
-            else:
-                if self.is_russian_vowel(word[i]):
-                    cnt += 1
+            if (prev == "" or not self.is_english_vowel(prev)) and self.is_english_vowel(word[i]):
+                cnt += 1
+            prev = word[i]
 
         return cnt
