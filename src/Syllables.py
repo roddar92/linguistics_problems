299afbae83c96f54234b5636f1c69c07d23accfa
roddar92 <rodionowadarja@yandex.ru>
2016-06-20 20:17:50 +0300

    SyllableModule --- create method with list of syllables

--- src/Syllables.py
+++ src/Syllables.py
@@ -2,11 +2,11 @@
 class SyllableModule(object):
     @staticmethod
     def is_russian_vowel(symbol):
-        return symbol in "аеиоуыэюя"
+        return symbol in "аеёиоуыэюя"
 
     @staticmethod
     def is_russian_consonant(symbol):
-        return symbol in "бвгджзйклмнпрстфчцчшщ"
+        return symbol in "бвгджзйклмнпрстфчцчшщъь"
 
     @staticmethod
     def is_english_vowel(symbol):
@@ -17,6 +17,8 @@ class SyllableModule(object):
         return symbol in "bcdfghjklmnpqrstvwxz"
 
     def russian_syllables_count(self, word):
+        """Retirn count of thw word syllables"""
+        word = word.lower()
         cnt = 0
         for letter in word:
             if self.is_russian_vowel(letter):
@@ -25,6 +27,7 @@ class SyllableModule(object):
         return cnt
 
     def english_syllables_count(self, word):
+        word = word.lower()
         leng = len(word)
         if word[-1] == "e":
             leng -= 1
@@ -37,3 +40,52 @@ class SyllableModule(object):
             prev = word[i]
 
         return cnt
+
+    def russian_syllables(self, word):
+        """Return list of the word syllables"""
+        word = word.lower()
+        if self.russian_syllables_count(word) <= 1:
+            return [word]
+
+        syllables = []
+        current_syllable = ""
+
+        for letter in word:
+            current_syllable += letter
+            if self.is_russian_vowel(letter):
+                syllables.append(current_syllable)
+                current_syllable = ""
+
+        if current_syllable:
+            last = syllables.pop(len(syllables) - 1)
+            syllables.append(last + current_syllable)
+
+        return syllables
+
+if __name__ == "__main__":
+    sm = SyllableModule()
+    assert sm.russian_syllables_count("Вова") == 2
+    assert sm.russian_syllables_count("Коронация") == 5
+    assert sm.russian_syllables_count("водоПад") == 3
+    assert sm.russian_syllables("Анфиса") == ["а", "нфи", "са"]
+    assert sm.russian_syllables("Петрополь") == ["пе", "тро", "поль"]
+    assert sm.russian_syllables_count("жест") == 1
+    assert sm.russian_syllables("жест") == ["жест"]
+    assert sm.russian_syllables("коронация") == ["ко", "ро", "на", "ци", "я"]
+    assert sm.russian_syllables_count("санаторий") == 4
+    assert sm.russian_syllables("санаторий") == ["са", "на", "то", "рий"]
+    assert sm.russian_syllables_count("Ломоносовская") == 6
+    assert sm.russian_syllables_count("Кошка") == 3
+    assert sm.russian_syllables("ласточка") == ["ла", "сто", "чка"]
+    assert sm.russian_syllables_count("Василеостровская") == 7
+    assert sm.russian_syllables_count("лапа") == 2
+    assert sm.russian_syllables("наледь") == ["на", "ледь"]
+    assert sm.russian_syllables("ветерочек") == ["ве", "те", "тро", "чек"]
+    assert sm.russian_syllables("кашалот") == ["ка", "ша", "лот"]
+    assert sm.russian_syllables_count("резюме") == 3
+    assert sm.russian_syllables("резюме") == ["ре", "зю", "ме"]
+    assert sm.russian_syllables_count("вечер") == 2
+    assert sm.russian_syllables_count("землетрясение") == 6
+    assert sm.russian_syllables("землетрясение") == ["зе", "мле", "тря", "се", "ни", "е"]
+    assert sm.russian_syllables_count("Исландия") == 4
+    assert sm.russian_syllables("Шотландия") == ["шо", "тла", "нди", "я"]
