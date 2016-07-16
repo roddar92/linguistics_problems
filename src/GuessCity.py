b8d1335ec51db3d5c2174c1c05ec35d12ad7bb8d
roddar92 <rodionowadarja@yandex.ru>
2016-07-16 23:24:17 +0300

    GuessCity --- create new game

---
+++ src/GuessCity.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+class GuessCity(object):
+    def __init__(self):
+        self.allowed_cities = set()
+        self.guessed_cities = set()
+        self.city_name = "Unknown"
+
+        with open("ru_cities.txt", "r", encoding='utf-8') as f:
+            for line in f:
+                name = line.strip()
+                self.allowed_cities.add(name)
+
+    @staticmethod
+    def get_city_name(city):
+        if '-' in city:
+            return "-".join([w[0].upper() + w[1:] for w in city.split("-")])
+        elif ' ' in city:
+            return " ".join([w[0].upper() + w[1:] for w in city.split()])
+        else:
+            return city[0].upper() + city[1:]
+
+    def move(self):
+        self.city_name = [word for word in self.allowed_cities if word not in self.guessed_cities]
+        print(self.city_name[0])
+
+    def shift_letter(self, index):
+        if index == len(self.city_name):
+            print("The guessed city was {}. Game is over :(".format(self.city_name))
+            self.move()
+        else:
+            print(self.city_name[:index + 1])
