8305ccd239c1f06e412355cda14a891b8b7e5c0e
roddar92 <rodionowadarja@yandex.ru>
2016-07-16 23:26:49 +0300

    Delete anagrams code

--- src/Anagrams.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-class AnagramOperations(object):
-    def is_anagram(self, word1, word2):
-        """Check is word1 the anagram of word2"""
-        if len(word1) != len(word2):
-            return False
-        return sorted(word1) == sorted(word2)
-
-    # todo create method for anagrams generation
