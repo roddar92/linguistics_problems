3e7594cc4f1cf04067c176c3c1d50160a88d1036
roddar92 <rodionowadarja@yandex.ru>
2017-02-27 20:00:49 +0300

    More or Less game --- Extend opportunities

--- src/More_or_Less.py
+++ src/More_or_Less.py
@@ -7,6 +7,11 @@ class MoreOrLessGame:
         self.number = -1
         self.numbers = set()
         self.quizzed_numbers = set()
+        self.left, self.right = 1, 100
+
+    def update_fields(self):
+        self.number = -1
+        self.left, self.right = 1, 100
 
     def clear_fields(self):
         self.number = -1
@@ -28,16 +33,25 @@ class MoreOrLessGame:
         if answer in self.numbers:
             raise Exception("Ты уже называл такое число.")
         elif answer > 100:
-            raise Exception("Я не загадывал число больше 100.")
+            raise Exception("Ой-ой-ой! Моё число не может быть больше 100.")
+        elif answer < self.left or answer > self.right:
+            raise Exception("Ладно, я тебе подскажу! :) Моё число находится в границах [%d, %d]."
+                            % (self.left, self.right))
         else:
             self.numbers.add(answer)
             if answer == self.number:
-                self.number = -1
+                self.update_fields()
                 self.quizzed_numbers.add(answer)
-                print("Ты угадал моё число. Молодец!")
-                game.move()
+                print("Ты угадал моё число. Молодец! Сыграешь со мной ещё?")
+                self.move()
             else:
-                print("Твоё число %s моего. Попробуешь ещё разок?" % ("больше" if answer > self.number else "меньше"))
+                if answer > self.number:
+                    ml = "больше"
+                    self.right = answer
+                else:
+                    ml = "меньше"
+                    self.left = answer
+                print("Твоё число %s моего. Попробуешь ещё разок?" % ml)
 
     def print_result(self):
         total = len(self.quizzed_numbers)
@@ -48,11 +62,11 @@ class MoreOrLessGame:
         else:
             word = "чисел"
         print("Тогда давай закончим игру! Ты угадал %d %s." % (total, word))
-        game.clear_fields()
+        self.clear_fields()
 
 
 def is_agree():
-    return s in "Да да Давай давай yes yes".split()
+    return s in "Да да Давай давай Yes yes".split()
 
 
 def is_end_of_game():
