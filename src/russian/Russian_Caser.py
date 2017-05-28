840103a43b86042d0a4de3e87bda5f720d1575c7
roddar92 <rodionowadarja@yandex.ru>
2017-05-28 23:15:21 +0300

    Russian Case --- split dative method for single and plural word form

--- src/russian/Russian_Caser.py
+++ src/russian/Russian_Caser.py
@@ -15,97 +15,101 @@ class RussianCaser(object):
     """ Return word in Russian dative case
         :param word is Russian lemma
         :param gen is gender of word, if gen is None then first word of non-defined gender, otherwise male of female
-        :param single defines single or plural form of lemma
         :return word in Russian dative case
-
-        !!! Method architecture has refactored due to incorrectly working with plural form !!!
     """
-    def dative(self, word, gen=None, single=True):
+    def dative_single(self, word, gen=None):
         if self.is_without_ending(word):
             return word
         else:
-            if single:
-                if gen == 'f':
-                    if word in ['мать', 'дочь']:
-                        return word[:-1] + 'ери'
-                    elif word.endswith('ожь'):
-                        if word == 'дрожь':
-                            return word[:-1] + 'и'
-                        else:
-                            return word[:-3] + 'жи'
-                    elif word[-1] == 'ь':
-                        return word[:-1] + 'и'
-                    elif word[-1] in 'ая':
-                        return word[:-1] + 'е'
-                elif gen == 'm':
-                    if word[-1] in 'йь':
-                        return word[:-1] + 'ю'
-                    elif word.endswith('чек'):
-                        return word[:-2] + 'ку'
-                    elif word.endswith('ёк'):
-                        return word[:-2] + 'ьку'
-                    elif word.endswith('ок') and word[-3] in 'нтчш' and len(word) > 4:
-                        return word[:-2] + 'ку'
-                    elif word.endswith('ец'):
-                        return word[:-2] + 'цу'
-                    else:
-                        return word + 'у'
-                else:
-                    return word[:-1] + 'у'
-            else:
+            if gen == 'f':
                 if word in ['мать', 'дочь']:
-                    return word[:-1] + 'ерям'
-                elif word in ['брат', 'стул']:
-                    return word + 'ьям'
-                elif word == 'сын':
-                    return word + 'овьям'
-                elif word == 'друг':
-                    return word + 'зьям'
-                elif word == 'дерево':
-                    return word[:-1] + 'ьям'
-                elif word[-1] in 'йь':
-                    return word[:-1] + 'ям'
-                elif not self.is_vowel(word[-1]):
-                    return word + 'ам'
+                    return word[:-1] + 'ери'
+                elif word.endswith('ожь'):
+                    if word == 'дрожь':
+                        return word[:-1] + 'и'
+                    else:
+                        return word[:-3] + 'жи'
+                elif word[-1] == 'ь':
+                    return word[:-1] + 'и'
+                elif word[-1] in 'ая':
+                    return word[:-1] + 'е'
+            elif gen == 'm':
+                if word[-1] in 'йь':
+                    return word[:-1] + 'ю'
+                elif word.endswith('чек') and word != 'чек':
+                    return word[:-2] + 'ку'
+                elif word.endswith('ёк'):
+                    return word[:-2] + 'ьку'
+                elif word.endswith('ок') and word[-3] in 'нтчш' and len(word) > 4:
+                    return word[:-2] + 'ку'
+                elif word.endswith('ец'):
+                    return word[:-2] + 'цу'
                 else:
-                    return word + 'м'
+                    return word + 'у'
+            else:
+                return word[:-1] + 'у'
+
+    """ Return word in Russian dative case
+            :param word is Russian lemma
+            :return word in Russian dative case
+
+            !!! Method architecture has refactored due to incorrectly working with plural form !!!
+        """
+    def dative_plural(self, word):
+        if word in ['мать', 'дочь']:
+            return word[:-1] + 'ерям'
+        elif word in ['брат', 'стул']:
+            return word + 'ьям'
+        elif word == 'сын':
+            return word + 'овьям'
+        elif word == 'друг':
+            return word + 'зьям'
+        elif word == 'дерево':
+            return word[:-1] + 'ьям'
+        elif word[-1] in 'йь':
+            return word[:-1] + 'ям'
+        elif not self.is_vowel(word[-1]):
+            return word + 'ам'
+        else:
+            return word + 'м'
 
 
 if __name__ == "__main__":
     caser = RussianCaser()
-    assert caser.dative('ложь', 'f') == "лжи"
-    assert caser.dative('рожь', 'f') == "ржи"
-    assert caser.dative('дрожь', 'f') == "дрожи"
-    assert caser.dative("ванна", 'f') == "ванне"
-    assert caser.dative("мать", 'f') == "матери"
-    assert caser.dative("мать", 'f', False) == "матерям"
-    assert caser.dative("дочь", 'f') == "дочери"
-    assert caser.dative("рать", 'f') == "рати"
-    assert caser.dative("верёвка", 'f') == "верёвке"
-    assert caser.dative("капля", 'f') == "капле"
-    assert caser.dative("ванна", 'f', False) == "ваннам"
-    assert caser.dative("капель", 'f') == "капели"
-    assert caser.dative("дверь", 'f', False) == "дверям"
-    assert caser.dative("апрель", 'm') == "апрелю"
-    assert caser.dative("молодец", 'm') == "молодцу"
-    assert caser.dative("замочек", 'm') == "замочку"
-    assert caser.dative("ключик", 'm') == "ключику"
-    assert caser.dative("горшок", 'm') == "горшку"
-    assert caser.dative("шок", 'm') == "шоку"
-    assert caser.dative("станок", 'm') == "станку"
-    assert caser.dative("завиток", 'm') == "завитку"
-    assert caser.dative("старичок", 'm') == "старичку"
-    assert caser.dative("лоток", 'm') == "лотку"
-    assert caser.dative("ток", 'm') == "току"
-    assert caser.dative("ларёк", 'm') == "ларьку"
-    assert caser.dative("харёк", 'm') == "харьку"
-    assert caser.dative("порок", 'm') == "пороку"
-    assert caser.dative("санаторий", 'm') == "санаторию"
-    assert caser.dative("санаторий", 'm', False) == "санаториям"
-    assert caser.dative("ковчег", 'm') == "ковчегу"
-    assert caser.dative("такт", 'm') == "такту"
-    assert caser.dative("село", 'n') == "селу"
-    assert caser.dative("ковчег", 'm', False) == "ковчегам"
-    assert caser.dative("брат", 'm', False) == "братьям"
-    assert caser.dative("дерево", 'm', False) == "деревьям"
-    assert caser.dative("пальто") == "пальто"
+    assert caser.dative_single('ложь', 'f') == "лжи"
+    assert caser.dative_single('рожь', 'f') == "ржи"
+    assert caser.dative_single('дрожь', 'f') == "дрожи"
+    assert caser.dative_single("ванна", 'f') == "ванне"
+    assert caser.dative_single("мать", 'f') == "матери"
+    assert caser.dative_plural("мать") == "матерям"
+    assert caser.dative_single("дочь", 'f') == "дочери"
+    assert caser.dative_single("рать", 'f') == "рати"
+    assert caser.dative_single("верёвка", 'f') == "верёвке"
+    assert caser.dative_single("капля", 'f') == "капле"
+    assert caser.dative_plural("ванна") == "ваннам"
+    assert caser.dative_single("капель", 'f') == "капели"
+    assert caser.dative_plural("дверь") == "дверям"
+    assert caser.dative_single("апрель", 'm') == "апрелю"
+    assert caser.dative_single("молодец", 'm') == "молодцу"
+    assert caser.dative_single("замочек", 'm') == "замочку"
+    assert caser.dative_single("чек", 'm') == "чеку"
+    assert caser.dative_single("ключик", 'm') == "ключику"
+    assert caser.dative_single("горшок", 'm') == "горшку"
+    assert caser.dative_single("шок", 'm') == "шоку"
+    assert caser.dative_single("станок", 'm') == "станку"
+    assert caser.dative_single("завиток", 'm') == "завитку"
+    assert caser.dative_single("старичок", 'm') == "старичку"
+    assert caser.dative_single("лоток", 'm') == "лотку"
+    assert caser.dative_single("ток", 'm') == "току"
+    assert caser.dative_single("ларёк", 'm') == "ларьку"
+    assert caser.dative_single("харёк", 'm') == "харьку"
+    assert caser.dative_single("порок", 'm') == "пороку"
+    assert caser.dative_single("санаторий", 'm') == "санаторию"
+    assert caser.dative_plural("санаторий") == "санаториям"
+    assert caser.dative_single("ковчег", 'm') == "ковчегу"
+    assert caser.dative_single("такт", 'm') == "такту"
+    assert caser.dative_single("село") == "селу"
+    assert caser.dative_plural("ковчег") == "ковчегам"
+    assert caser.dative_plural("брат") == "братьям"
+    assert caser.dative_plural("дерево") == "деревьям"
+    assert caser.dative_single("пальто") == "пальто"
