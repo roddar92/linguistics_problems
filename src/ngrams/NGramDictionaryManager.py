7d577ef46ded87ef90dec68cca301dbacbf628d8
roddar92 <rodionowadarja@yandex.ru>
2017-06-02 22:31:25 +0300

    NGram Dictionary --- create dictionary manager

---
+++ src/ngrams/NGramDictionaryManager.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+from collections import defaultdict
+import os
+
+
+class NGramDictionaryManager(object):
+    def __init__(self):
+        self.ngram_dictionary = defaultdict(int)
+
+    def create_dictionary(self, input_path, n_gram_length=2, remove_short_words=False):
+
+        def get_ngrams(word, ngram_len):
+            n_grams = list()
+            for i in range(len(word) - ngram_len + 1):
+                n_grams.append(word[i:i + ngram_len])
+            return n_grams
+
+        with open(input_path, 'r', encoding='utf-8') as f:
+            for line in f:
+
+                # TODO tokenization with NLTK RegexpTokenizer(r'[\s\.\,:;\!\?\-\+=/\"&\*\(\)^\[\]\{\}]')
+                line = line.replace(',', "")
+                line = line.replace('.', "")
+                line = line.replace(':', "")
+                line = line.replace(';', "")
+                line = line.replace('!', "")
+                line = line.replace('?', "")
+
+                line = line.replace('+', "")
+                line = line.replace('-', "")
+                line = line.replace('*', "")
+                line = line.replace('/', "")
+                line = line.replace('=', "")
+                line = line.replace('&', "")
+                line = line.replace('^', "")
+
+                line = line.replace('(', "")
+                line = line.replace(')', "")
+                line = line.replace('[', "")
+                line = line.replace(']', "")
+                line = line.replace('<', "")
+                line = line.replace('>', "")
+
+                tokens = line.strip().split()
+
+                for token in tokens:
+                    if '\'' in token:
+                        token = token.replace('\'', "")
+                    token = token.lower()
+                    if len(token) <= n_gram_length and not remove_short_words:
+                        self.ngram_dictionary[token] += 1
+                    else:
+                        ngrams = get_ngrams(token, n_gram_length)
+                        for ngram in ngrams:
+                            self.ngram_dictionary[ngram] += 1
+
+    def save_dictionary_to_file(self, output_path):
+        with open(output_path, "w", encoding='utf-8') as f:
+            for n_gram in sorted(self.ngram_dictionary, key=lambda x: x[1]):
+                f.write(n_gram + "-" + str(self.ngram_dictionary[n_gram]))
+            f.flush()
+            f.close()
+
+    def print_dictionary(self, limit=10):
+        i = 0
+        for n_gram in sorted(self.ngram_dictionary, key=lambda x: x[1]):
+            if i < limit:
+                print(n_gram, "-", self.ngram_dictionary[n_gram])
+                i += 1
+            else:
+                break
+
+
+if __name__ == "__main__":
+    path = "resources"
+    ngram_length = 2
+
+    ngram_dict_manager = NGramDictionaryManager()
+    for descriptor in os.listdir(path):
+        ngram_dict_manager.create_dictionary(os.path.join(path, descriptor), 2, True)
+    ngram_dict_manager.print_dictionary()
