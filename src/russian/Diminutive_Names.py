ec22aa5683feb7fe896ff28146e63732d682fd4c
roddar92 <rodionowadarja@yandex.ru>
2017-09-23 00:32:17 +0300

    Diminutiver --- fix diminutive algorithm

--- src/russian/Diminutive_Names.py
+++ src/russian/Diminutive_Names.py
@@ -2,7 +2,7 @@
 class Diminutiver(object):
     @staticmethod
     def is_vowel(symbol):
-        return symbol in "аеиоуыэя"
+        return symbol in "аеиоуыэюя"
 
     @staticmethod
     def is_sibilant(symbol):
@@ -10,28 +10,33 @@ class Diminutiver(object):
 
     @staticmethod
     def is_endings_with_a(symbol):
-        return symbol in "бдвлмнпрст"
+        return symbol in "бвдзлмнпрст"
 
     def get_diminutive(self, name):
+        if name[-2:] == "ка":
+            if not self.is_vowel(name[-3]):
+                name = name[:-3] + "я" if name[-3] == "ь" else name[:-2] + "а"
+            else:
+                return name[:-1] + "уся"
+        elif name[-2:] == "ия":
+            name = name[:-2] + "я"
+
         suffix = ""
         if not self.is_vowel(name[-1]):
             if name[-1] == "ь":
                 return name[:-1] + "ёк"
             return name + "ик"
         elif name[-1] == "а":
-            if name[-2] == "к":
-                if not self.is_vowel(name[-3]):
-                    return "Cannot parse names with suffix '{}'!".format("К")
-                return name[:-1] + "уся"
+            if self.is_endings_with_a(name[-2]):
+                suffix = "очк"
             else:
-                if self.is_endings_with_a(name[-2]):
-                    suffix = "очк"
-                else:
-                    suffix = "еньк"
+                suffix = "еньк"
         elif name[-1] == "я":
             if name[-2] == "ь":
                 suffix = "юшк" if name[-3] not in "лмн" else "юш"
-            elif name[-2] == "н":
+            elif name[-2] == "л":
+                suffix = "ечк" if not self.is_vowel(name[0].lower()) else "еньк"
+            elif name[-2] in "мн":
                 suffix = "ечк"
             else:
                 suffix = "еньк"
@@ -48,6 +53,7 @@ if __name__ == "__main__":
     assert diminutiver.get_diminutive("Анфиса") == "Анфисочка"
     assert diminutiver.get_diminutive("Женя") == "Женечка"
     assert diminutiver.get_diminutive("Тима") == "Тимочка"
+    assert diminutiver.get_diminutive("Манька") == "Манечка"
     assert diminutiver.get_diminutive("Таня") == "Танечка"
     assert diminutiver.get_diminutive("Клава") == "Клавочка"
     assert diminutiver.get_diminutive("Клара") == "Кларочка"
@@ -56,16 +62,21 @@ if __name__ == "__main__":
     assert diminutiver.get_diminutive("Влад") == "Владик"
     assert diminutiver.get_diminutive("Вика") == "Викуся"
     assert diminutiver.get_diminutive("Витя") == "Витенька"
-    assert diminutiver.get_diminutive("Витька") == "Cannot parse names with suffix '{}'!".format("К")
+    assert diminutiver.get_diminutive("Васька") == "Васенька"
     assert diminutiver.get_diminutive("Диана") == "Дианочка"
     assert diminutiver.get_diminutive("Дина") == "Диночка"
     assert diminutiver.get_diminutive("Люда") == "Людочка"
-    assert diminutiver.get_diminutive("Людка") == "Cannot parse names with suffix '{}'!".format("К")
+    assert diminutiver.get_diminutive("Лиза") == "Лизочка"
+    assert diminutiver.get_diminutive("Людка") == "Людочка"
     assert diminutiver.get_diminutive("Люба") == "Любочка"
     assert diminutiver.get_diminutive("Влада") == "Владочка"
     assert diminutiver.get_diminutive("Лампа") == "Лампочка"
-    assert diminutiver.get_diminutive("Лавка") == "Cannot parse names with suffix '{}'!".format("К")
+    assert diminutiver.get_diminutive("Лавка") == "Лавочка"
     assert diminutiver.get_diminutive("Дамба") == "Дамбочка"
     assert diminutiver.get_diminutive("Дарья") == "Дарьюшка"
+    assert diminutiver.get_diminutive("Галя") == "Галечка"
+    assert diminutiver.get_diminutive("Емеля") == "Емеленька"
+    assert diminutiver.get_diminutive("Лилия") == "Лилечка"
+    assert diminutiver.get_diminutive("Юлия") == "Юленька"
     assert diminutiver.get_diminutive("Илья") == "Ильюша"
     assert diminutiver.get_diminutive("Игорь") == "Игорёк"
