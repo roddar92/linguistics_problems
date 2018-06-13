50a7f213336b7165943b10bdb744404dfc19b3dc
roddar92 <rodionowadarja@yandex.ru>
2018-06-14 00:12:34 +0300

    RuTokenizer -- Override a naive tokenizer with automated programming

--- src/russian/NaiveTokenizer.py
+++ src/russian/NaiveTokenizer.py
@@ -1,4 +1,4 @@
-import re, string
+import re
 
 
 class NaiveTokenizer(object):
@@ -9,37 +9,62 @@ class NaiveTokenizer(object):
                 line.strip().lower() for line in inf
             ]
 
-        # self.domains = []
+        with open('../russian/resources/tokenizer/tlds-alpha-by-domain.txt', 'r') as inf:
+            self.tlds = [
+                line.strip().lower() for i, line in enumerate(inf) if i > 3
+            ]
+
+        url = r'('
+        url += r'(?:(https?|s?ftp):\/\/)?'
+        url += r'(?:www\.)?'
+        url += r'('
+        url += r'(?:(?:([A-Z0-9][A-Z0-9-_]+)@([A-Z0-9-_\.])+[A-Z0-9]\.)' \
+               r'|(?:[A-Z0-9][A-Z0-9-]{0,61}[A-Z0-9]\.)+)'
+
+        tld = r'(' + self.tlds[0]
+        for i in range(1, len(self.tlds)):
+            tld += r'|{}'.format(self.tlds[i])
+        tld += r')'
+
+        url += tld
+        url += r'|(?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+        url += r')'
+        url += r'(?::(\d{1,5}))?'
+        url += r'(?:(\/[^\.\,\s]+))?'
+        url += r')'
+
+        self.URL = re.compile(url, re.IGNORECASE)
         self.CURRENCY = '$€£¢¥₽'
-        self.DIGIT = re.compile(r'(\d)+([\.,](\d)+)?')
+        self.OTHER_PUNCT = '#%'
+        self.DIGIT = re.compile(r'((\d)+([.,](\d)+)?)')
         self.EOS = '.?!'
         self.INS = ',:;'
-        self.QUOTES = '\"\'\`'
+        self.QUOTES = '\"\'\`«»“”‘’'
         self.LBRACKETS = '([{'
         self.RBRACKETS = ')]}'
 
     def tokenize(self, text):
 
-        def get_sequence(token):
+        def get_sequence(cur_token):
 
-            START, CURRENCY, EOS, BRACK, WORD = range(5)
+            START, CURRENCY, EOS, INS, WORD = range(5)
 
             toks = []
             cur_tok = ''
             state = START
 
-            for c in token:
+            for c in cur_token:
                 if c in self.QUOTES + self.LBRACKETS + self.RBRACKETS + self.INS:
                     if state == START:
-                        state = BRACK
+                        state = INS
                         cur_tok += c
-                    elif state in (EOS, BRACK, CURRENCY, WORD):
-                        state = BRACK
+                    elif state in (EOS, INS, CURRENCY, WORD):
+                        state = INS
                         if cur_tok:
-                            toks += cur_tok
-                            cur_tok += c
+                            toks += [cur_tok]
+                            cur_tok = c
                     else:
-                        state = BRACK
+                        state = INS
                         cur_tok += c
                 elif c in self.EOS:
                     if state == START:
@@ -47,49 +72,47 @@ class NaiveTokenizer(object):
                         cur_tok += c
                     elif state == EOS:
                         cur_tok += c
-                    elif state in (BRACK, CURRENCY):
+                    elif state in (INS, CURRENCY):
                         state = EOS
                         if cur_tok:
-                            toks += cur_tok
-                            cur_tok += c
+                            toks += [cur_tok]
+                            cur_tok = c
                     elif state == WORD:
                         state = EOS
-                        if cur_tok and cur_tok + c not in self.abbr:
-                            toks += [cur_tok]
+                        if cur_tok and ((cur_tok + c).lower() in self.abbr or len(cur_tok) <= 1):
                             cur_tok += c
                         else:
-                            cur_tok += c
+                            toks += [cur_tok]
+                            cur_tok = c
                     else:
                         state = EOS
                         cur_tok += c
-                elif c in self.CURRENCY or \
-                        (c in string.punctuation and c not in self.QUOTES +
-                         self.LBRACKETS + self.RBRACKETS + self.INS + self.EOS):
+                elif c in self.CURRENCY or c in self.OTHER_PUNCT:
                     if state == START:
                         state = CURRENCY
                         cur_tok += c
                     elif state == CURRENCY:
                         cur_tok += c
-                    elif state in (EOS, BRACK, WORD):
+                    elif state in (EOS, INS, WORD):
                         state = CURRENCY
                         if cur_tok:
-                            toks += cur_tok
-                            cur_tok += c
+                            toks += [cur_tok]
+                            cur_tok = c
                     else:
                         state = CURRENCY
                         cur_tok += c
                 elif c.isspace():
                     if cur_tok:
-                        toks += cur_tok
+                        toks += [cur_tok]
                 else:
                     if state == START:
                         state = WORD
                         cur_tok += c
-                    elif state in (EOS, BRACK, CURRENCY):
+                    elif state in (EOS, INS, CURRENCY):
                         state = WORD
                         if cur_tok:
-                            toks += cur_tok
-                            cur_tok += c
+                            toks += [cur_tok]
+                            cur_tok = c
                     elif state == WORD:
                         cur_tok += c
                     else:
@@ -103,8 +126,9 @@ class NaiveTokenizer(object):
 
         for excpected_token in text.split():
             if self.URL.search(excpected_token) or self.DIGIT.search(excpected_token):
-                start, end = self.URL.search(excpected_token).start(), self.URL.search(excpected_token).end()
-                if not (start or end):
+                if self.URL.search(excpected_token):
+                    start, end = self.URL.search(excpected_token).start(), self.URL.search(excpected_token).end()
+                else:
                     start, end = self.DIGIT.search(excpected_token).start(), \
                                  self.DIGIT.search(excpected_token).end()
                 first, middile, last = excpected_token[:start], excpected_token[start:end], excpected_token[end:]
@@ -119,27 +143,6 @@ class NaiveTokenizer(object):
                 for token in get_sequence(excpected_token):
                     yield token
 
-            """if excpected_token[0] in self.CURRENCY:
-            yield excpected_token[0]
-            excpected_token = excpected_token[1:]
-
-        if excpected_token.endswith(','):
-                yield excpected_token[:-1]
-                yield excpected_token[-1]
-            elif ',' in excpected_token and not self.DIGIT.match(excpected_token):
-                ind = excpected_token.find(',')
-                yield excpected_token[:ind]
-                yield excpected_token[ind]
-                yield excpected_token[ind + 1:]
-            elif excpected_token.endswith('.') and \
-                    (excpected_token in self.abbr or len(excpected_token[:-1]) <= 1):
-                yield excpected_token
-            elif excpected_token[-1] in self.EOS + self.INS:
-                yield excpected_token[:-1]
-                yield excpected_token[-1]
-            else:
-                yield excpected_token"""
-
 
 if __name__ == '__main__':
     tokenizer = NaiveTokenizer()
@@ -147,12 +150,12 @@ if __name__ == '__main__':
     assert list(tokenizer.tokenize(
         'Курс доллара на сегодняшний день составляет 62.73, курс евро - 73,73.'
     )) == ['Курс', 'доллара', 'на', 'сегодняшний', 'день', 'составляет', '62.73', ',',
-          'курс', 'евро', '-', '73,73', '.']
+           'курс', 'евро', '-', '73,73', '.']
 
     assert list(tokenizer.tokenize(
         'В г. Санкт-Петербург ожидаются дожди с грозами, температура составит около 2-х градусов тепла.'
     )) == ['В', 'г.', 'Санкт-Петербург', 'ожидаются', 'дожди', 'с', 'грозами', ',',
-          'температура', 'составит', 'около', '2-х', 'градусов', 'тепла', '.']
+           'температура', 'составит', 'около', '2', '-х', 'градусов', 'тепла', '.']
 
     assert list(tokenizer.tokenize(
         'В огороде бузина, а в Киеве - дядька.'
@@ -161,3 +164,21 @@ if __name__ == '__main__':
     assert list(tokenizer.tokenize(
         'Нефть стоит $50,67. А в деревне Гадюкино - снова дожди!'
     )) == ['Нефть', 'стоит', '$', '50,67', '.', 'А', 'в', 'деревне', 'Гадюкино', '-', 'снова', 'дожди', '!']
+
+    assert list(tokenizer.tokenize(
+        'Нефть стоит $50,67 . А в Кишенёве (моём родном городе) гостит моя тётка ...'
+    )) == ['Нефть', 'стоит', '$', '50,67', '.', 'А', 'в', 'Кишенёве', '(', 'моём', 'родном',
+           'городе', ')', 'гостит', 'моя', 'тётка', '...']
+
+    assert list(tokenizer.tokenize(
+        'Следите за всеми новостями тут : facebook.com/zebrochka.'
+    )) == ['Следите', 'за', 'всеми', 'новостями', 'тут', ':', 'facebook.com/zebrochka', '.']
+
+    assert list(tokenizer.tokenize(
+        'Пишите все письма на мой электронный ящик : dobro@gmail.com.'
+    )) == ['Пишите', 'все', 'письма', 'на', 'мой', 'электронный', 'ящик', ':', 'dobro@gmail.com', '.']
+
+    assert list(tokenizer.tokenize(
+        'Следите за новостями здесь : facebook.com/zebrochka.Подписывайтесь на наш "канал" и оставляйте лайки !!!'
+    )) == ['Следите', 'за', 'новостями', 'здесь', ':', 'facebook.com/zebrochka', '.',
+           'Подписывайтесь', 'на', 'наш', '"', 'канал', '"', 'и', 'оставляйте', 'лайки', '!!!']
