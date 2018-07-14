c35a9259b367153cfecd2862138ac07923b1f376
roddar92 <rodionowadarja@yandex.ru>
2018-07-15 00:56:18 +0300

    PosTagger -- Add new rules to naive pos-tagger

--- src/russian/NaivePosTagger.py
+++ src/russian/NaivePosTagger.py
@@ -46,45 +46,39 @@ def is_punctuation(seq):
     return all([j in string.punctuation for j in seq])
 
 
-def has_affixes(seq):
-    return seq[0] in 'kodpsvn' or seq[:2] in ['za', 'iz'] or seq[:3] in ['bez', 'bes']
-
-
 def features(sequence, i):
     """
-        Функция, которая отображает входы сразу в фичи
+        Generate features from inputs
 
-        :param sequence: набор колонок
-        :param i: номер слова
-        :return: множество фич
+        :param sequence: columns set
+        :param i: word number
+        :return: features set
     """
     seq = sequence[i].split("\t")[1]
 
-    # позиция номер один в предложении
+    # first position in the sentence
     if i == 0:
         yield "first"
 
     if i == len(sequence) - 1:
         yield "last"
 
-    # длина слова, если оно короче 5 символов
+    # word's length
     yield "len=" + get_word_len(seq)
 
-    # первые три буквы слова
+    # first 4 letters
     yield "first_four_letters=" + seq[:4] if len(seq) > 4 else seq
 
-    # последние три буквы слова
+    # last 3 letters
     yield "last_three_letters=" + seq[-3:] if len(seq) > 3 else seq
 
     # word shape
     yield "word_shape=" + str(get_word_shape(seq))
     yield "short_word_shape=" + get_short_word_shape(seq)
 
-    # начинается с прописной
     if seq.istitle():
         yield "title"
 
-    # все буквы прописные
     if seq.isupper():
         yield "upper"
 
@@ -94,111 +88,112 @@ def features(sequence, i):
     if '-' in seq:
         yield "has_dash"
 
-    # количество цифр
     yield "digits_count=" + str(digits_count(seq))
 
-    # имеет приставки
-    if has_affixes(seq):
-        yield "starts_with_affixes"
+    # only alpha or digit
+    if seq.isalpha():
+        yield "alpha"
 
-    # состоит только из букв и чисел
-    if seq.isalnum():
-        yield "alnum"
-
-    # состоит только из цифр
+    # only digits
     if real_num_pattern.search(seq) or seq.isdigit():
         yield "num"
 
-    # число с валютой
+    # currency
     if currency_pattern.search(seq):
         yield "currency"
 
-    # последняя буква - гласная
-    if is_vowel(seq[-1]):
-        yield "last_vowel=" + seq[-2:] \
-            if re.search(r'y[au]$', seq) or (len(seq) >= 2 and is_vowel(seq[-2])) else seq[-1]
+    # contains -'its'
+    if 'its' in seq or re.search(r'\w+(tel|nik)', seq):
+        yield "with_tel_its"
 
-    # содержит -'иц'
-    if 'its' in seq or re.search(r'\w+nik', seq):
-        yield "with_its"
+    # contains letter + 'к' suffix
+    if re.search(r'\w+[bjlmnpstvz]k', seq):
+        yield "with_k_suffix"
 
-    # содержит некоторые суффиксы
-    if re.search(r'\w+[stz][kn]', seq):
-        yield "with_sk_suffix"
+    # contains letter + 'в' suffix
+    if re.search(r'\w+(st|z|o)v', seq):
+        yield "with_v_suffix"
 
-    if re.search(r'\w+st?v', seq):
-        yield "with_stv_suffix"
+    if re.search(r'\w+[eio]k', seq):
+        yield "with_eiok_suffix"
 
-    if re.search(r'\w+ov', seq):
-        yield "with_ov_suffix"
+    if re.search(r'\w+stn', seq):
+        yield "with_stn_suffix"
 
-    if re.search(r'\w+vn', seq):
-        yield "with_vn_suffix"
+    if re.search(r'\w+[dk]r', seq):
+        yield "with_dr_suffix"
 
-    if re.search(r'\w+[ei]k', seq):
-        yield "with_eik_suffix"
+    if re.search(r'\w+(sh|jj)k', seq):
+        yield "with_shk_suffix"
 
-    # содержит 'нн'
-    if re.search(r'\w+[ei]nn', seq) or 'nn' in seq:
+    # contains 'нн'
+    if 'nn' in seq:
         yield "with_nn"
 
-    # содержит 'чн', 'чк'
+    # contains 'чн', 'чк'
     if 'chk' in seq or 'chn' in seq or 'schn' in seq:
         yield "with_chk"
 
-    # содержит 'нк'
-    if 'nk' in seq:
-        yield "with_nk"
+    # contains letter + 'н' suffix
+    if re.search(r'\w+[jlmrstvz]n', seq):
+        yield "with_n_suffix"
 
-    if re.search(r'l[aeio]?', seq, re.IGNORECASE):
+    # ends with 'л' and similar endings
+    if re.search(r'l[aeio]?$', seq):
         yield "ends_with_l"
 
-    # содержит суффиксы 'ющ', 'ящ', 'ищ', 'вш'
-    if re.search(r'\w+(y[aiu]sch|vsh)', seq) or seq.endswith('v'):
+    # contains suffixes 'ющ', 'ящ', 'ищ', 'вш'
+    if re.search(r'\w+((y[au]|i)s?ch|vsh)', seq) or seq.endswith('v'):
         yield "with_part_sch_suffixes"
 
-    # слово оканчивается на 'ся'
+    # ends with 'ся'
     if seq.endswith("sya") or seq.endswith('s\''):
         yield "ends_with_sya"
 
     if seq.endswith('j'):
         yield "ends_with_j"
 
-    # последняя буква - согласная
-    if seq.endswith('\'') and not is_vowel(seq[-2]):
-        yield "ends_with_apo"
-
     if 'w' in seq.lower():
         yield "with_w_letter"
 
     if i > 0:
         prev = sequence[i - 1].split("\t")[1]
-        # предыдущая с прописной
+        # previous word's length
+        yield "prev_len=" + str(get_word_len(prev))
+
+    if i > 0:
+        prev = sequence[i - 1].split("\t")[1]
+        # previous word is title
         yield "prev_title=" + str(prev.istitle())
 
     if i > 0:
         prev = sequence[i - 1].split("\t")[1]
-        # последние буквы предыдущей
+        # last letters of the previous word
         yield "prev_last_letters=" + (prev[-3:] if len(prev) > 3 else prev)
 
     if i > 0:
         prev = sequence[i - 1].split("\t")[1]
-        # предыдущая состоит из букв и цифр
+        # previous word is alnum
         yield "prev_is_alnum=" + str(prev.isalnum())
 
     if i < len(sequence) - 1:
         next = sequence[i + 1].split("\t")[1]
-        # следующая с прописной
+        # next word's length
+        yield "next_len=" + str(get_word_len(next))
+
+    if i < len(sequence) - 1:
+        next = sequence[i + 1].split("\t")[1]
+        # next word is title
         yield "next_title=" + str(next.istitle())
 
     if i < len(sequence) - 1:
         next = sequence[i + 1].split("\t")[1]
-        # последние буквы следующей
+        # last letters of the next word
         yield "next_last_letters=" + (next[-3:] if len(next) > 3 else next)
 
     if i < len(sequence) - 1:
         next = sequence[i + 1].split("\t")[1]
-        # следующая состоит из букв и цифр
+        # next word is alnum
         yield "next_is_alnum=" + str(next.isalnum())
 
 # читаем обучающее множество
