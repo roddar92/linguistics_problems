44c8692d0f350df625d22fb575eccc698be09574
roddar92 <rodionowadarja@yandex.ru>
2018-10-03 16:18:23 +0300

    Morphemer -- Minor changes in the morphemes classifier

--- src/russian/morphemes/scripts/hmm/viterbi_classifier.py
+++ src/russian/morphemes/scripts/hmm/viterbi_classifier.py
@@ -7,21 +7,19 @@ class HMMClassifier:
         self.start_states, self.trans, self.emis = HMMModelHandler.normalize_model(start_states, trans, emis)
         self.max_word_len = max(map(len, [t[0] for raw in self.emis for t in raw.keys()]))
 
-    def find_morphemes(self, word, pos_tag):
-        # must return the best combination of morphemes (used Viterbi algorithm)
+    def find_the_best_partition(self, word, pos_tag):
+        # return the best combination of morphemes (used Viterbi algorithm)
         def t_prob(s1, s2):
             state_prob = self.trans.get(s1, {})
-            if state_prob:
-                return state_prob.get(s2, 0)
-            else:
+            if not state_prob:
                 return 0
+            return state_prob.get(s2, 0)
 
         def e_prob(w, pos, s):
             state_prob = self.emis.get(s, {})
-            if state_prob:
-                return state_prob.get((w, pos), 0)
-            else:
+            if not state_prob:
                 return 0
+            return state_prob.get((w, pos), 0)
 
         def s_prob(s):
             return self.start_states.get(s, 0)
@@ -33,7 +31,7 @@ class HMMClassifier:
         probs.append(prob)
         states.append(st)
         lasts.append(k)
-        for j in (k, len(word) + 1):
+        for j in range(k, len(word) + 1):
             prob, k, st = max(probs[t] * (e_prob(word[t:j], pos_tag, st) * t_prob(states[t], st), k, st)
                               for t in range(max(0, len(word) - self.max_word_len))
                               for st in HMMModelHandler.get_states())
