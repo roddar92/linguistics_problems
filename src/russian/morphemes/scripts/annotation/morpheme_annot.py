4090f914e32c9af13dc023a4ab74d525e129b5b7
roddar92 <rodionowadarja@yandex.ru>
2018-10-03 13:27:36 +0300

    Morphemer -- Minor changes in the annotator (MORPHS)

--- src/russian/morphemes/scripts/annotation/morpheme_annot.py
+++ src/russian/morphemes/scripts/annotation/morpheme_annot.py
@@ -12,11 +12,12 @@ NOUN_REPLACEMENT_RULES = {
     re.compile(r'(ая|ай|ий|ой|ый|ые|ою|ия|ие|ии|ию|ей|ую|ее|ое)$'): r'[\1/END]',
     re.compile(r'(ом|ов|ам|ем|им|ым)\['): r'[\1/END][',
     re.compile(r'(ом|ов|ам|ем|им|ым|ах|их|ых)$'): r'[\1/END]',
-    re.compile(r'(знь)$'): r'[\1/SFX]',
-    re.compile(r'([аеёи]нн?|нн?)\['): r'[\1/SFX][',
-    re.compile(r'(ович|ость|ост|ичн|тель?|ниц|ник|щик|ств|ск|ек|ик|ок|чк|оч|чн|ич|яч|зн|ст|сн)\['): r'[\1/SFX][',
+    re.compile(r'(?<!^)(знь)$'): r'[\1/SFX]',
+    re.compile(r'(?<!^)([аеёи]нн?|нн?)\['): r'[\1/SFX][',
+    re.compile(r'(?<!^)(ович|ость|ост|ичн|тель?|ниц|ник|щик|енк|ньк|ств|ск|ек|ик|ок|чк|оч|чн|ич|яч|зн|ст|сн|к)\['):
+        r'[\1/SFX][',
     re.compile(r'(ен|ов)\['): r'[\1/IFX][',
-    re.compile(r'^(не|во[зс]|пере|бе[зс]|пр[иео]|обо?|анти|пред|под|вы|по|ра[зс]|о|за|из|до|от)'): r'[\1/AFX]',
+    re.compile(r'^(не|во[зс]|пере|бе[зс]|пр[иео]|обо?|анти|пред|под|вы|по|ра[зс]|у|о|за|из|до|от)'): r'[\1/AFX]',
     re.compile(r'\]([^\[\]]+)\['): r'][\1/RT][',
     re.compile(r'^([^\[\]]+)\['): r'[\1/RT]['
 }
@@ -30,29 +31,29 @@ VERB_REPLACEMENT_RULES = {
     re.compile(r'([уюаяеи]шь)\['): r'[\1/END][',
     re.compile(r'(ть)\['): r'[\1/END][',
     re.compile(r'(ть)$'): r'[\1/END]',
-    re.compile(r'([аеиоуыэюя])\[ть'): r'[\1/SFX][ть',
+    re.compile(r'(?<!^)([аеиоуыэюя])\[ть'): r'[\1/SFX][ть',
     re.compile(r'(?<![аеиоуыэюя])([аеиоуыэюя])$'): r'[\1/END]',
     re.compile(r'(?<![аеиоуыэюя])([аеиоуыэюя])\['): r'[\1/END][',
     re.compile(r'(я)$'): r'[\1/SFX]',
-    re.compile(r'([уюаяи]щ|вш|енн|инн|нн)\['): r'[\1/SFX][',
-    re.compile(r'([аяиеыё]л)\['): r'[\1/SFX][',
-    re.compile(r'([аяиеыё]л)$'): r'[\1/SFX]',
-    re.compile(r'([нкш])\['): r'[\1/SFX][',
-    re.compile(r'(ом|ов|ам|ем|им|ым)$'): r'[\1/SFX]',
-    re.compile(r'(ич)\['): r'[\1/SFX][',
+    re.compile(r'(?<!^)([уюаяи]щ|вш|енн|инн|нн)\['): r'[\1/SFX][',
+    re.compile(r'(?<!^)([аяиеыё]л)\['): r'[\1/SFX][',
+    re.compile(r'(?<!^)([аяиеыё]л)$'): r'[\1/SFX]',
+    re.compile(r'(?<!^)([нкш])\['): r'[\1/SFX][',
+    re.compile(r'(?<!^)(ом|ов|ам|ем|им|ым)$'): r'[\1/SFX]',
+    re.compile(r'(?<!^)(ич)\['): r'[\1/SFX][',
     re.compile(r'^(не|во[зс]|пере|бе[зс]|пр[иео]|об|анти|пред|под|вы|по|ра[зс]|о|за|из|до|от)'): r'[\1/AFX]',
     re.compile(r'\]([^\[\]]+)\['): r'][\1/RT][',
     re.compile(r'^([^\[\]]+)\['): r'[\1/RT]['
 }
 
 
-EXCEPTED_POS = ['CONJ', 'PR', 'NONLEX', 'S-PRO', 'ADV-PRO', 'PART']
+EXCEPTED_POS = ['CONJ', 'PR', 'NONLEX', 'S-PRO', 'ADV-PRO', 'PART', 'PRAEDIC']
 
 
 def apply_rules(word_tuple):
     word, pos = word_tuple
     word = re.sub(r'ё', 'е', word)
-    rules = NOUN_REPLACEMENT_RULES if pos in 'S A A-PRO'.split() else VERB_REPLACEMENT_RULES
+    rules = NOUN_REPLACEMENT_RULES if pos in 'S A A-PRO NUM'.split() else VERB_REPLACEMENT_RULES
     for replacement_cond, replacement_rule in rules.items():
         word = replacement_cond.sub(replacement_rule, word)
 
