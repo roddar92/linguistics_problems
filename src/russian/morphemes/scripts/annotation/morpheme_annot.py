2bd71b6c9ae14af3ec8a766988c1232e270d571a
roddar92 <rodionowadarja@yandex.ru>
2018-10-03 18:03:47 +0300

    Morphemer -- Minor changes for morpheme manual annotator and create the annotation2label converter

--- src/russian/morphemes/scripts/annotation/morpheme_annot.py
+++ src/russian/morphemes/scripts/annotation/morpheme_annot.py
@@ -2,48 +2,56 @@ import os
 import re
 
 
+RT = r'([^\[\]]+)'
 ROOT = re.compile(r'(.+)')
-
+NOUN_SUFFIXES = r'(ович|ость|ост|ичн|тель?|ниц|ник|щик|енк|ньк|ств|ск|ек|ик|ок|чк|оч|чн|ич|яч|зн|ст|сн|к)'
+PAST_VB_SFX = r'([аяиеыё]л)'
+ENDING = r'(?<![аеиоуыэюя])([аеиоуыэюя])'
+ADJ_ENDINGS = r'(ая|ай|ий|ой|ый|ые|ою|ия|ие|ии|ию|ей|ую|ее|ое)'
+VERB_ENDINGS = r'([уюаяеи]те?)'
+AFFIX = r'(не|во[зс]|пере|бе[зс]|пр[иео]|обо?|анти|пред|под|вы|по|ра[зс]|у|о|за|из|до|от)'
 
 NOUN_REPLACEMENT_RULES = {
-    re.compile(r'(ого|его)$'): r'[\1/END]',
-    re.compile(r'(?<![аеиоуыэюя])([аеиоуыэюя])$'): r'[\1/END]',
-    re.compile(r'(?<![аеиоуыэюя])([аеиоуыэюя])\['): r'[\1/END][',
-    re.compile(r'(ая|ай|ий|ой|ый|ые|ою|ия|ие|ии|ию|ей|ую|ее|ое)$'): r'[\1/END]',
-    re.compile(r'(ом|ов|ам|ем|им|ым)\['): r'[\1/END][',
-    re.compile(r'(ом|ов|ам|ем|им|ым|ах|их|ых)$'): r'[\1/END]',
+    re.compile(r'(?<!^)' + r'(ого|его)$'): r'[\1/END]',
+    re.compile(r'(?<!^)' + ENDING + r'$'): r'[\1/END]',
+    re.compile(r'(?<!^)' + ENDING + r'\['): r'[\1/END][',
+    re.compile(r'(?<!^)' + ADJ_ENDINGS + r'$'): r'[\1/END]',
+    re.compile(r'(?<!^)(ом|ов|ам|ем|им|ым)\['): r'[\1/END][',
+    re.compile(r'(?<!^)(ом|ов|ам|ем|им|ым|ах|их|ых)$'): r'[\1/END]',
     re.compile(r'(?<!^)(знь)$'): r'[\1/SFX]',
     re.compile(r'(?<!^)([аеёи]нн?|нн?)\['): r'[\1/SFX][',
-    re.compile(r'(?<!^)(ович|ость|ост|ичн|тель?|ниц|ник|щик|енк|ньк|ств|ск|ек|ик|ок|чк|оч|чн|ич|яч|зн|ст|сн|к)\['):
+    re.compile(r'(?<!^)' + NOUN_SUFFIXES + '\['):
         r'[\1/SFX][',
+    re.compile(r'(?<!^)' + NOUN_SUFFIXES + '$'):
+        r'[\1/SFX]',
     re.compile(r'(ен|ов)\['): r'[\1/IFX][',
-    re.compile(r'^(не|во[зс]|пере|бе[зс]|пр[иео]|обо?|анти|пред|под|вы|по|ра[зс]|у|о|за|из|до|от)'): r'[\1/AFX]',
-    re.compile(r'\]([^\[\]]+)\['): r'][\1/RT][',
-    re.compile(r'^([^\[\]]+)\['): r'[\1/RT]['
+    re.compile(r'^' + AFFIX + r'(?!$)'): r'[\1/AFX]',
+    re.compile(r'\]' + RT + r'\['): r'][\1/RT][',
+    re.compile(r'^' + RT + r'\['): r'[\1/RT]['
 }
 
 
 VERB_REPLACEMENT_RULES = {
-    re.compile(r'(с[ья])$'): r'[\1/PSFX]',
-    re.compile(r'([уюаяеи]те?)$'): r'[\1/END]',
-    re.compile(r'([уюаяеи]те?)\['): r'[\1/END][',
-    re.compile(r'([уюаяеи]шь)$'): r'[\1/END]',
-    re.compile(r'([уюаяеи]шь)\['): r'[\1/END][',
-    re.compile(r'(ть)\['): r'[\1/END][',
-    re.compile(r'(ть)$'): r'[\1/END]',
+    re.compile(r'(?<!^)' + r'(с[ья])$'): r'[\1/PSFX]',
+    re.compile(r'(?<!^)' + VERB_ENDINGS + r'$'): r'[\1/END]',
+    re.compile(r'(?<!^)' + VERB_ENDINGS + r'\['): r'[\1/END][',
+    re.compile(r'(?<!^)' + r'([уюаяеи]шь)$'): r'[\1/END]',
+    re.compile(r'(?<!^)' + r'([уюаяеи]шь)\['): r'[\1/END][',
+    re.compile(r'(?<!^)' + r'(ть)\['): r'[\1/END][',
+    re.compile(r'(?<!^)' + r'(ть)$'): r'[\1/END]',
     re.compile(r'(?<!^)([аеиоуыэюя])\[ть'): r'[\1/SFX][ть',
-    re.compile(r'(?<![аеиоуыэюя])([аеиоуыэюя])$'): r'[\1/END]',
-    re.compile(r'(?<![аеиоуыэюя])([аеиоуыэюя])\['): r'[\1/END][',
-    re.compile(r'(я)$'): r'[\1/SFX]',
+    re.compile(r'(?<!^)' + ENDING + r'$'): r'[\1/END]',
+    re.compile(r'(?<!^)' + ENDING + r'\['): r'[\1/END][',
+    re.compile(r'(?<!^)' + r'(я)$'): r'[\1/SFX]',
     re.compile(r'(?<!^)([уюаяи]щ|вш|енн|инн|нн)\['): r'[\1/SFX][',
-    re.compile(r'(?<!^)([аяиеыё]л)\['): r'[\1/SFX][',
-    re.compile(r'(?<!^)([аяиеыё]л)$'): r'[\1/SFX]',
+    re.compile(r'(?<!^)' + PAST_VB_SFX + '\['): r'[\1/SFX][',
+    re.compile(r'(?<!^)' + PAST_VB_SFX + r'$'): r'[\1/SFX]',
     re.compile(r'(?<!^)([нкш])\['): r'[\1/SFX][',
     re.compile(r'(?<!^)(ом|ов|ам|ем|им|ым)$'): r'[\1/SFX]',
     re.compile(r'(?<!^)(ич)\['): r'[\1/SFX][',
-    re.compile(r'^(не|во[зс]|пере|бе[зс]|пр[иео]|об|анти|пред|под|вы|по|ра[зс]|о|за|из|до|от)'): r'[\1/AFX]',
-    re.compile(r'\]([^\[\]]+)\['): r'][\1/RT][',
-    re.compile(r'^([^\[\]]+)\['): r'[\1/RT]['
+    re.compile(r'^' + AFFIX + r'(?!$)'): r'[\1/AFX]',
+    re.compile(r'\]' + RT + r'\['): r'][\1/RT][',
+    re.compile(r'^' + RT + r'\['): r'[\1/RT]['
 }
 
 
@@ -51,11 +59,15 @@ EXCEPTED_POS = ['CONJ', 'PR', 'NONLEX', 'S-PRO', 'ADV-PRO', 'PART', 'PRAEDIC']
 
 
 def apply_rules(word_tuple):
+    def is_long_seq(word):
+        return len(word) > 3
+
     word, pos = word_tuple
     word = re.sub(r'ё', 'е', word)
     rules = NOUN_REPLACEMENT_RULES if pos in 'S A A-PRO NUM'.split() else VERB_REPLACEMENT_RULES
-    for replacement_cond, replacement_rule in rules.items():
-        word = replacement_cond.sub(replacement_rule, word)
+    if is_long_seq(word):
+        for replacement_cond, replacement_rule in rules.items():
+            word = replacement_cond.sub(replacement_rule, word)
 
     if '[' not in word or ']' not in word:
         word = ROOT.sub(r'[\1/RT]', word)
@@ -67,8 +79,8 @@ def annotate_morphemes(input_dir, output_dir, path_to_filename):
             open(os.path.join(output_dir, path_to_filename), 'w', encoding='utf-8') as f2:
         for line in f1.readlines():
             t = tuple(eval(line.strip()))
-            if t[-1] in 'SAV' and '-' in t[0]:
-                if t[-1] in 'SV':
+            if t[-1] in 'S A V ADV'.split() and '-' in t[0]:
+                if t[-1] in 'S V ADV'.split():
                     for w in t[0].split('-'):
                         index = None
                         if re.search(r'([.,?!…]+)', w):
---
+++ src/russian/morphemes/scripts/evaluation/utils.py
@@ -0,0 +1,53 @@
+import os
+import re
+from collections import Counter
+
+
+STATES = ['AFX', 'RT', 'IFX', 'SFX', 'END', 'PSFX']
+ALL_EXCLUDE_BRACKETS = r'([^\[\]]+)'
+ANNOT = r'\[' + ALL_EXCLUDE_BRACKETS + r'/(' + r'|'.join(STATES) + r')\]'
+ANNOT_PATTERN = re.compile(ANNOT, re.IGNORECASE)
+
+
+def annot2label(input_dir, output_dir):
+    out_descr = open(os.path.join(output_dir, 'label_statistics.txt'), 'w', encoding='utf-8')
+    for f in os.listdir(input_dir):
+        input_file = os.path.join(input_dir, f)
+        output_file = os.path.join(output_dir, f)
+        with open(input_file, 'r', encoding='utf-8') as fin, \
+                open(output_file, 'w', encoding='utf-8') as fout:
+
+            all_pairs_count, labels_count = 0, 0
+            pos_tags_counter = Counter()
+
+            for line in fin.readlines():
+                word, pos = tuple(eval(line.strip()))
+                res = []
+                for elem in ANNOT_PATTERN.finditer(word):
+                    morph, morph_type = elem.group(1), elem.group(2)
+                    res += ['{}-{}'.format(morph, morph_type)]
+                if res:
+                    fout.write(','.join(res) + '\t' + pos)
+                    fout.write('\n')
+                    labels_count += 1
+                pos_tags_counter[pos] += 1
+                all_pairs_count += 1
+            fout.flush()
+            out_descr.write('{}: Processed all {} lines and labels - {}'.format(
+                input_file, all_pairs_count, labels_count))
+            out_descr.write('\n')
+            out_descr.write('POS TAGS:')
+            out_descr.write('\n')
+            for pos, count in pos_tags_counter.most_common():
+                out_descr.write('{} - {}'.format(pos, count))
+                out_descr.write('\n')
+            out_descr.write('\n')
+            out_descr.flush()
+    if out_descr:
+        out_descr.close()
+
+
+if __name__ == '__main__':
+    INPUT_DIR = '../../resources/corpora/annotated'
+    OUTPUT_DIR = '../../resources/corpora/true_labels'
+    annot2label(INPUT_DIR, OUTPUT_DIR)
