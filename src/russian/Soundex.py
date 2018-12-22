aef77ad21eef7cd747d3c32370453a6045711961
roddar92 <rodionowadarja@yandex.ru>
2018-12-22 23:33:36 +0300

    Soundex -- Minor changes

--- src/russian/Soundex.py
+++ src/russian/Soundex.py
@@ -1,6 +1,7 @@
 import re
 from abc import ABC, abstractmethod
 import editdistance
+import pymorphy2
 
 
 class Soundex(ABC):
@@ -88,6 +89,7 @@ class EnglishSoundex(Soundex):
 class RussianSoundex(Soundex):
     _vowels = 'аэиоуыеёюя'
     _table = str.maketrans('бпвфгкхдтжшчщзсцлмнр', '11223334455556667889')
+    _ego_ogo_endings = re.compile(r'([ео])(г)(о$)', re.IGNORECASE)
 
     _replacement_map = {
         re.compile(r'(^|ъ|ь|' + r'|'.join(_vowels) + r')(я)', re.IGNORECASE): 'jа',
@@ -107,7 +109,35 @@ class RussianSoundex(Soundex):
         re.compile(r'([дт][зсц])', re.IGNORECASE): 'ц'
     }
 
+    def __init__(self, delete_first_letter=False, delete_first_coded_letter=False,
+                 delete_zeros=False, cut_result=False, seq_cutted_len=4,
+                 use_morph_analysis=False):
+        """
+        Initialization of Russian Soundex object
+        :param delete_first_letter:
+        :param delete_first_coded_letter:
+        :param delete_zeros:
+        :param cut_result:
+        :param seq_cutted_len:
+        :param use_morph_analysis: use morphological grammems for phonemes analysis
+        """
+        super(RussianSoundex, self).__init__(delete_first_letter, delete_first_coded_letter,
+                         delete_zeros, cut_result, seq_cutted_len)
+        self.use_morph_analysis = use_morph_analysis
+        self._moprh = pymorphy2.MorphAnalyzer()
+
+    def _replace_ego_ogo_endings(self, word):
+        return self._ego_ogo_endings.sub(r'\1в\3', word)
+
+    def _use_morph_for_phoneme_replace(self, word):
+        parse = self._moprh.parse(word)
+        if parse and ('ADJF' in parse[0].tag or 'NUMB' in parse[0].tag or 'NPRO' in parse[0].tag):
+            word = self._replace_ego_ogo_endings(word)
+        return word
+
     def transform(self, word):
+        if self.use_morph_analysis:
+            word = self._use_morph_for_phoneme_replace(word)
         for replace, result in self._replacement_map.items():
             word = replace.sub(result, word)
         return self._apply_soundex_algorithm(word)
@@ -166,11 +196,16 @@ if __name__ == '__main__':
     assert ru_soundex.transform('счастье') == 'Ч5064J0'
     assert ru_soundex.transform('агенство') == ru_soundex.transform('агентство')
     assert ru_soundex.transform('театр') == ru_soundex.transform('тятр')
-    assert ru_soundex.transform('ненасный') == ru_soundex.transform('ненастный')
     assert ru_soundex.transform('сонце') == ru_soundex.transform('солнце')
     assert ru_soundex.transform('серце') == ru_soundex.transform('сердце')
     assert ru_soundex.transform('считать') == 'Ч50404'
     assert ru_soundex.transform('щитать') == 'Щ50404'
+
+    ru_soundex = RussianSoundex(use_morph_analysis=True)
+    assert ru_soundex.transform('зелёного') == 'З60708020'
+    assert ru_soundex.transform('никого') == 'Н803020'
+    assert ru_soundex.transform('ничего') == 'Н805020'
+    assert ru_soundex.transform('много') == 'М8030'
 
     ru_soundex = RussianSoundex(delete_first_letter=True)
     similarity_checker = SoundexSimilarity(ru_soundex)
@@ -181,4 +216,6 @@ if __name__ == '__main__':
     assert similarity_checker.similarity('ночь', 'мочь') == 0
     assert similarity_checker.similarity('сахар', 'цукер') == 0
     assert similarity_checker.similarity('булочная', 'булошная') == 0
-    assert similarity_checker.similarity('мальчёнка', 'мальчонка') == 0
+    assert similarity_checker.similarity('булочная', 'булошная') == 0
+    assert similarity_checker.similarity('блеснуть', 'блестнуть') == 0
+    assert similarity_checker.similarity('ненасный', 'ненастный') == 0
