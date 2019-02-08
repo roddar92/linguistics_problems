4fd6124beddf7d9e3af21b3c1506e96a25684ae1
roddar92 <rodionowadarja@yandex.ru>
2019-02-08 23:21:59 +0300

    Ngram -- Refactoring code

--- src/ngrams/NGramDictionaryManager.py
+++ src/ngrams/NGramDictionaryManager.py
@@ -1,46 +1,41 @@
 # -*- coding: utf-8 -*-
 import os
 import re
-from collections import defaultdict, deque
+import string
+from collections import Counter, deque
 
 from nltk.corpus import stopwords
 
 
-class NGramDictionaryManager(object):
+class NGramDictionaryManager:
     def __init__(self):
         self.stop_words = set(stopwords.words('english') + stopwords.words('russian'))
-        self.ngram_dictionary = defaultdict(int)
+        self.ngram_dictionary = Counter()
 
     def clear_dictionary(self):
         self.ngram_dictionary.clear()
 
     def text_preprocessing(self, line, remove_stop_words):
         line = line.strip()
-        line = re.sub(r'[\r\t\n\.,:;!\'\"\?_\-\+=/&\*\(\)\^\[\]\{\}\<\>\|]', u'', line)
+        line = re.sub(r'[\r\t\n' + string.punctuation + r']', ' ', line)
         tokens = [token.lower() for token in line.split()]
         if remove_stop_words:
-            tokens = [token for token in tokens if token not in self.stop_words]
+            tokens = filter(lambda t: t not in self.stop_words, tokens)
         return tokens
 
     # TODO override this method for information retrieval with spell-checker
     def create_dictionary_for_spelling(self, input_path, n_gram_length=2, remove_stop_words=False):
         def get_ngrams(word, ngram_len):
-            n_grams = list()
             for i in range(len(word) - ngram_len + 1):
-                n_grams.append(word[i:i + ngram_len])
-            return n_grams
+                yield word[i:i + ngram_len]
 
         with open(input_path, 'r', encoding='utf-8') as f:
             for line in f:
-                tokens = self.text_preprocessing(line, remove_stop_words)
-
-                for token in tokens:
+                for token in self.text_preprocessing(line, remove_stop_words):
                     if '\'' in token:
-                        token = token.replace('\'', "")
-                    token = token.lower()
+                        token = re.sub(r'\'', '', token)
                     if len(token) >= n_gram_length:
-                        ngrams = get_ngrams(token, n_gram_length)
-                        for ngram in ngrams:
+                        for ngram in get_ngrams(token.lower(), n_gram_length):
                             self.ngram_dictionary[ngram] += 1
 
     def create_dictionary_for_translation(self, input_path, n_gram_length=2, remove_stop_words=False):
@@ -48,16 +43,16 @@ class NGramDictionaryManager(object):
             tokens = self.text_preprocessing(f.read(), remove_stop_words)
 
             n_gram = deque()
-            i = 0
             for k in range(n_gram_length - 1):
-                n_gram.append(tokens[i])
-                i += 1
+                n_gram.append(next(tokens))
 
-            while i < len(tokens):
-                n_gram.append(tokens[i])
+            try:
+                while True:
+                    n_gram.append(next(tokens))
+                    self.ngram_dictionary[' '.join(n_gram)] += 1
+                    n_gram.popleft()
+            except StopIteration:
                 self.ngram_dictionary[' '.join(n_gram)] += 1
-                n_gram.popleft()
-                i += 1
 
     def save_dictionary_to_file(self, output_path):
         with open(output_path, "w", encoding='utf-8') as f:
@@ -67,13 +62,8 @@ class NGramDictionaryManager(object):
             f.close()
 
     def print_dictionary(self, limit=10):
-        i = 0
-        for (k, v) in sorted(self.ngram_dictionary.items(), key=lambda x: -x[1]):
-            if i < limit:
-                print("{}\t{}".format(k, v))
-                i += 1
-            else:
-                break
+        for k, v in self.ngram_dictionary.most_common(limit):
+            print("{}\t{}".format(k, v))
 
 
 if __name__ == "__main__":
@@ -82,16 +72,17 @@ if __name__ == "__main__":
     for descriptor in os.listdir(path):
         ngram_dict_manager = NGramDictionaryManager()
 
-        path_desc = os.path.join(path, descriptor)
-        print(path_desc)
-        print('Spelling N-grams')
-        ngram_dict_manager.create_dictionary_for_spelling(path_desc, 2, remove_stop_words=True)
-        ngram_dict_manager.print_dictionary()
+        if descriptor.endswith('.txt'):
+            path_desc = os.path.join(path, descriptor)
+            print(path_desc)
+            print('Spelling N-grams')
+            ngram_dict_manager.create_dictionary_for_spelling(path_desc, 2, remove_stop_words=True)
+            ngram_dict_manager.print_dictionary()
 
-        print()
-        ngram_dict_manager.clear_dictionary()
+            print()
+            ngram_dict_manager.clear_dictionary()
 
-        print('Translation N-grams')
-        ngram_dict_manager.create_dictionary_for_translation(path_desc, 2, remove_stop_words=True)
-        ngram_dict_manager.print_dictionary()
-        print('\n')
+            print('Translation N-grams')
+            ngram_dict_manager.create_dictionary_for_translation(path_desc, 2, remove_stop_words=True)
+            ngram_dict_manager.print_dictionary()
+            print('\n')
