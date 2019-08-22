7c3acb86ed9610929ae5d06ac1145fb3587884cf
Dashka Rodionova <rodionowadarja@yandex.ru>
2019-08-22 14:30:24 +0300

    Diminutive_Names --- code refactoring

--- src/russian/Diminutive_Names.py
+++ src/russian/Diminutive_Names.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 class Diminutiver(object):
     @staticmethod
-    def is_vowel(symbol):
+    def __is_vowel(symbol):
         return symbol in "аеиоуыэюя"
 
     @staticmethod
-    def is_sibilant(symbol):
+    def __is_sibilant(symbol):
         return symbol in "чшщ"
 
     @staticmethod
-    def is_endings_with_a(symbol):
+    def __is_endings_with_a(symbol):
         return symbol in "бвдзлмнпрст"
 
     def get_diminutive(self, name):
         if name.endswith("ка"):
-            if not self.is_vowel(name[-3]):
+            if not self.__is_vowel(name[-3]):
                 name = name[:-3] + "я" if name[-3] in "йь" else name[:-2] + "а"
             else:
                 return name[:-1] + "уся"
@@ -24,7 +24,7 @@ class Diminutiver(object):
             name = name[:-1] + "я"
 
         suffix = ""
-        if not self.is_vowel(name[-1]):
+        if not self.__is_vowel(name[-1]):
             if name[-1] == "й":
                 return name[:-1] + "юшка"
             elif name[-1] == "ь":
@@ -42,7 +42,7 @@ class Diminutiver(object):
             else:
                 return name
         elif name[-1] == "а":
-            if self.is_endings_with_a(name[-2]):
+            if self.__is_endings_with_a(name[-2]):
                 suffix = "ушк" if name[-3] == "ё" else "очк"
             else:
                 suffix = "еньк"
@@ -50,7 +50,7 @@ class Diminutiver(object):
             if name[-2] == "ь":
                 suffix = "юшк" if name[-3] not in "лмн" else "юш"
             elif name[-2] == "л":
-                suffix = "ечк" if not self.is_vowel(name[0].lower()) else "еньк"
+                suffix = "ечк" if not self.__is_vowel(name[0].lower()) else "еньк"
             elif name[-2] in "аемн":
                 suffix = "ечк"
             else:
