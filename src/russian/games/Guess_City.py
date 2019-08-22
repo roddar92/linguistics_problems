1bf21210ad0d4604089d1f849c426e3a8c7b03b7
Dashka Rodionova <rodionowadarja@yandex.ru>
2019-08-22 13:51:35 +0300

    GuessCity --- code refactoring

--- src/russian/games/Guess_City.py
+++ src/russian/games/Guess_City.py
@@ -10,20 +10,20 @@ class GuessCityGameException(Exception):
 
 class GuessCity(object):
     def __init__(self):
-        self.allowed_cities = defaultdict(list)
-        self.guessed_cities = set()
-        self.asked_cities = set()
-        self.city_name = "Unknown"
+        self.__allowed_cities = defaultdict(list)
+        self.__guessed_cities = set()
+        self.__asked_cities = set()
+        self.__city_name = "Unknown"
 
-        self.whole_moves_count = 0
-        self.attempts = 0
-        self.ind = 1
+        self.__whole_moves_count = 0
+        self.__attempts = 0
+        self.__ind = 1
 
         with open("../resources/ru_cities.txt", "r", encoding='utf-8') as f:
             for city in f:
                 city = city.strip()
                 if city:
-                    self.allowed_cities[city[0]] += [city]
+                    self.__allowed_cities[city[0]].append(city)
 
     @staticmethod
     def get_city_name(city_name):
@@ -35,13 +35,13 @@ class GuessCity(object):
             return city_name.title()
 
     def print_final_text(self):
-        if self.whole_moves_count <= 5:
+        if self.__whole_moves_count <= 5:
             print("WOW! You are lucky!")
-        elif 5 < self.whole_moves_count <= 8:
+        elif 5 < self.__whole_moves_count <= 8:
             print("You have a great intuition!")
-        elif 8 < self.whole_moves_count <= 12:
+        elif 8 < self.__whole_moves_count <= 12:
             print("Very nice work!")
-        elif 12 < self.whole_moves_count <= 18:
+        elif 12 < self.__whole_moves_count <= 18:
             print("Not bad...")
         else:
             print("How long you have to solve puzzles!.. But not upset! :) Get ready for the next time!")
@@ -50,76 +50,76 @@ class GuessCity(object):
         self.move()
         return input("Your answer: ").lower()
 
-    def get_hint(self):
-        self.ind += 1
-        self.attempts = 0
-        if self.ind == len(self.city_name):
-            self.ind = 1
-            gc.choose_city()
+    def __get_hint(self):
+        self.__ind += 1
+        self.__attempts = 0
+        if self.__ind == len(self.__city_name):
+            self.__ind = 1
+            self.choose_city()
         else:
             print("Well, I shall prompt you another letter!")
-            gc.shift_letter(self.ind)
+            self.__shift_letter(self.__ind)
 
     def get_guessed_city(self):
-        return self.city_name
+        return self.__city_name
 
-    def _make_city_used(self):
-        self.guessed_cities.add(self.city_name)
-        self.city_name = ""
+    def __make_city_used(self):
+        self.__guessed_cities.add(self.city_name)
+        self.__city_name = ""
 
-    def make_city_asked(self, city_name):
-        self.asked_cities.add(city_name)
+    def __make_city_asked(self, city_name):
+        self.__asked_cities.add(city_name)
 
     def reset_current_params(self):
-        self._make_city_used()
-        self._clear_asked_cities()
+        self.__make_city_used()
+        self.__clear_asked_cities()
 
-        self.whole_moves_count = 0
-        self.attempts = 0
-        self.ind = 1
+        self.__whole_moves_count = 0
+        self.__attempts = 0
+        self.__ind = 1
     
-    def _clear_asked_cities(self):
-        self.asked_cities.clear()
+    def __clear_asked_cities(self):
+        self.__asked_cities.clear()
 
-    def _was_city_asked(self, city_name):
-        return city_name in self.asked_cities
+    def __was_city_asked(self, city_name):
+        return city_name in self.__asked_cities
 
-    def _was_city_guessed(self, city_name):
-        return city_name in self.guessed_cities
+    def __was_city_guessed(self, city_name):
+        return city_name in self.__guessed_cities
 
-    def _is_city_exists(self, city_name):
-        return city_name in self.allowed_cities[city_name[0]]
+    def __is_city_exists(self, city_name):
+        return city_name in self.__allowed_cities[city_name[0]]
+    
+    def __shift_letter(self, index):
+        if index == len(self.__city_name):
+            print(f"The guessed city was {self.__city_name}. This game was over :(")
+        else:
+            print(self.__city_name[:index] + "...")
 
     def check_city(self, city_name):
-        if self._was_city_asked(city_name):
+        if self.__was_city_asked(city_name):
             raise GuessCityGameException("You have already asked this city!")
-        elif self._was_city_guessed(city_name):
+        elif self.__was_city_guessed(city_name):
             raise GuessCityGameException("This city has already guessed!")
-        elif not self._is_city_exists(city_name):
+        elif not self.__is_city_exists(city_name):
             raise GuessCityGameException("This city was not exists!")
 
     def move(self):
-        random_letter = random.choice(list(self.allowed_cities.keys()))
-        city_names = [word for word in self.allowed_cities[random_letter] if word not in self.guessed_cities]
-        self.city_name = self.get_city_name(random.choice(city_names))
-        print(f"My guessed city is {self.city_name[0]}...")
-
-    def shift_letter(self, index):
-        if index == len(self.city_name):
-            print(f"The guessed city was {self.city_name}. This game was over :(")
-        else:
-            print(self.city_name[:index] + "...")
+        random_letter = random.choice(list(self.__allowed_cities.keys()))
+        city_names = [word for word in self.__allowed_cities[random_letter] if word not in self.__guessed_cities]
+        self.__city_name = self.get_city_name(random.choice(city_names))
+        print(f"My guessed city is {self.__city_name[0]}...")
 
     def generate_fail_answer(self, city=None, catched_ex=None):
-        if self.attempts == 2:
-            self.get_hint()
+        if self.__attempts == 2:
+            self.__get_hint()
         elif catched_ex:
-            self.attempts += 1
+            self.__attempts += 1
             print(e)
             print("Try to guess my city again!")
         else:
-            self.make_city_asked(city)
-            self.attempts += 1
+            self.__make_city_asked(city)
+            self.__attempts += 1
             print(f"No... It isn't {self.get_city_name(city)} :( Try to guess my city again!")
 
 
