346d9fd5cd52990410e20940f519bdbafeb450f0
Dashka Rodionova <rodionowadarja@yandex.ru>
2019-08-23 16:58:00 +0300

    Transliterator --- code refactoring

--- src/russian/NaiveTransliterator.py
+++ src/russian/NaiveTransliterator.py
@@ -29,16 +29,16 @@ class Transliterator:
     }
 
     def __init__(self, need_spell=False):
-        self.RU_VOWELS = 'аeёиоуыэюя'
-        self.AFFIXES = re.compile(
+        self.__RU_VOWELS = 'аeёиоуыэюя'
+        self.__AFFIXES = re.compile(
             r'(^|\s+)(pod|raz|iz|pred|ot|ob|bez|in|trans|(sver|dvu|tr([yj][ёo]|ё))k?h)$',
             re.IGNORECASE
         )
-        self.E_AFFIX = re.compile(r'(^|\s+)[Ss]$', re.IGNORECASE)
-        self.CH_START_REGEX = re.compile(r'(^|\s+)ch$', re.IGNORECASE)
-        self.CH_END_REGEX = re.compile(r'^ch($|\s+|[' + string.punctuation + r']+)', re.IGNORECASE)
+        self.__E_AFFIX = re.compile(r'(^|\s+)[Ss]$', re.IGNORECASE)
+        self.__CH_START_REGEX = re.compile(r'(^|\s+)ch$', re.IGNORECASE)
+        self.__CH_END_REGEX = re.compile(r'^ch($|\s+|[' + string.punctuation + r']+)', re.IGNORECASE)
 
-        self.PHONEMES = {
+        self.__PHONEMES = {
             'я': ['ya', 'ia', 'ja', 'â'],
             'ю': ['yu', 'iu', 'ju', 'û'],
             'э': ['ae', 'ė'],
@@ -74,161 +74,161 @@ class Transliterator:
             'ы': ['y']
         }
 
-        self.COMPLEX_PHONEMES = {
+        self.__COMPLEX_PHONEMES = {
             'jog': ['й', 'о', 'г'],
             'yog': ['й', 'о', 'г'],
             'tch': ['т', 'ч'],
             'tsh': ['т', 'ш']
         }
 
-        self.UNCORRECTED_PHONEMES = {
+        self.__UNCORRECTED_PHONEMES = {
             re.compile(r'ця', re.IGNORECASE): 'тся',
             re.compile(r'цч', re.IGNORECASE): 'тщ',
             re.compile(r'шч', re.IGNORECASE): 'щ',
             re.compile(r'([жш])(ы)', re.IGNORECASE): r'\1и',
-            re.compile(r'([' + self.RU_VOWELS + r'])ы', re.IGNORECASE): r'\1й',
+            re.compile(r'([' + self.__RU_VOWELS + r'])ы', re.IGNORECASE): r'\1й',
             re.compile(r'([чщ])(я)', re.IGNORECASE): r'\1а',
             re.compile(r'([чщ])(ю)', re.IGNORECASE): r'\1у'
         }
 
-        self.COMBINATED_PHONEMES = {
+        self.__COMBINATED_PHONEMES = {
             'ie': 'ие',
             'je': 'ье',
             'ye': 'ые'
         }
 
-        self.COMBINATED_E_PHONEMES = {
+        self.__COMBINATED_E_PHONEMES = {
             'io': 'ио',
             'jo': 'йо',
             'yo': 'йо'
         }
 
-        for phoneme, val in list(self.COMBINATED_PHONEMES.items()):
-            self.COMBINATED_PHONEMES[phoneme.capitalize()] = val.capitalize()
+        for phoneme, val in list(self.__COMBINATED_PHONEMES.items()):
+            self.__COMBINATED_PHONEMES[phoneme.capitalize()] = val.capitalize()
 
-        for phoneme, val in list(self.COMBINATED_E_PHONEMES.items()):
-            self.COMBINATED_E_PHONEMES[phoneme.capitalize()] = val.capitalize()
+        for phoneme, val in list(self.__COMBINATED_E_PHONEMES.items()):
+            self.__COMBINATED_E_PHONEMES[phoneme.capitalize()] = val.capitalize()
 
-        self.need_spell = need_spell
-        self.tokenizer = None
-        self.spell_checker = None
+        self.__need_spell = need_spell
+        self.__tokenizer = None
+        self.__spell_checker = None
 
-        if need_spell:
+        if self.__need_spell:
             pass
-            # self.tokenizer = NaiveTokenizer()
-            # self.spell_checker = StatisticalSpeller()
+            # self.__tokenizer = NaiveTokenizer()
+            # self.__spell_checker = StatisticalSpeller()
 
-        for phoneme in self.PHONEMES.copy():
-            if not self.is_solid_or_soft_sign(phoneme):
-                self.PHONEMES[phoneme.capitalize()] = [ph.capitalize() for ph in self.PHONEMES[phoneme]]
+        for phoneme in self.__PHONEMES.copy():
+            if not self.__is_solid_or_soft_sign(phoneme):
+                self.__PHONEMES[phoneme.capitalize()] = [ph.capitalize() for ph in self.__PHONEMES[phoneme]]
 
-        self.straight_phonemes = {k: v[0] for k, v in self.PHONEMES.items()}
-        self.inverted_phonemes = {t: k for k, v in self.PHONEMES.items() for t in v}
+        self.__straight_phonemes = {k: v[0] for k, v in self.__PHONEMES.items()}
+        self.__inverted_phonemes = {t: k for k, v in self.__PHONEMES.items() for t in v}
 
-        self.inverted_phonemes['x'] = 'кс'
-        self.inverted_phonemes['qu'] = 'кв'
-        self.inverted_phonemes['iy'] = 'ый'
-        self.inverted_phonemes['ij'] = 'ый'
+        self.__inverted_phonemes['x'] = 'кс'
+        self.__inverted_phonemes['qu'] = 'кв'
+        self.__inverted_phonemes['iy'] = 'ый'
+        self.__inverted_phonemes['ij'] = 'ый'
 
-        self.inverted_phonemes.update(self.COMPLEX_PHONEMES)
+        self.__inverted_phonemes.update(self.__COMPLEX_PHONEMES)
 
-        self.keys = str.maketrans(self.straight_phonemes)
+        self.__keys = str.maketrans(self.__straight_phonemes)
 
-    def _is_shch_exceptions(self, word):
+    def __is_shch_exceptions(self, word):
         return word in self.SHCH_EXCEPTIONS
 
     @staticmethod
-    def is_solid_or_soft_sign(letter):
+    def __is_solid_or_soft_sign(letter):
         return letter in 'ъь'
 
     @staticmethod
-    def _is_of_word_start(elems, i):
+    def __is_of_word_start(elems, i):
         return i == 0 or not elems[-1].isalpha()
 
-    def _has_s_affix(self, text, i):
-        return text[i:i + 2] in ['ie', 'ye', 'je'] and self.E_AFFIX.search(text[:i])
+    def __has_s_affix(self, text, i):
+        return text[i:i + 2] in ['ie', 'ye', 'je'] and self.__E_AFFIX.search(text[:i])
 
-    def _iot_must_changed(self, text, i):
+    def __iot_must_changed(self, text, i):
         return text[i:i + 2].lower() in self.IOT_VOWELS and text[i + 2] in 'tdga'
+    
+    def __starts_with_affix(self, text):
+        return self.__AFFIXES.search(text)
 
-    def _is_solid_sign_possible(self, text, i):
-        return self._starts_with_affix(text[:i]) or self._has_s_affix(text, i)
+    def __is_solid_sign_possible(self, text, i):
+        return self.__starts_with_affix(text[:i]) or self.__has_s_affix(text, i)
 
-    def _is_iot_combination(self, charseq):
+    def __is_iot_combination(self, charseq):
         return charseq.lower() in self.IAT_VOWELS + self.IET_VOWELS + self.IUT_VOWELS + self.IOT_VOWELS
 
-    def _is_vowel(self, character):
-        return character.lower() in self.RU_VOWELS
+    def __is_vowel(self, character):
+        return character.lower() in self.__RU_VOWELS
 
-    def _starts_with_affix(self, text):
-        return self.AFFIXES.search(text)
-
-    def _transliterate_ij_ending(self, text, elems, i):
+    def __transliterate_ij_ending(self, text, elems, i):
         symbol = text[i]
-        if symbol in 'i' and self._is_vowel(elems[-1]):
+        if symbol in 'i' and self.__is_vowel(elems[-1]):
             return 'й'
-        elif text[i] in self.inverted_phonemes:
-            return self.inverted_phonemes[symbol]
+        elif text[i] in self.__inverted_phonemes:
+            return self.__inverted_phonemes[symbol]
         else:
             return symbol
 
-    def _transliterate_vowels_sequence(self, text, answer, elems, i):
-        if self._is_of_word_start(elems, i) or self._is_vowel(elems[-1]) or elems[-1] == 'ь':
-            if self._is_of_word_start(elems, i) and self._iot_must_changed(text, i):
-                return self.COMBINATED_E_PHONEMES[text[i:i + 2]]
-            return self.inverted_phonemes[text[i:i + 2]]
+    def __transliterate_vowels_sequence(self, text, answer, elems, i):
+        if self.__is_of_word_start(elems, i) or self.__is_vowel(elems[-1]) or elems[-1] == 'ь':
+            if self.__is_of_word_start(elems, i) and self.__iot_must_changed(text, i):
+                return self.__COMBINATED_E_PHONEMES[text[i:i + 2]]
+            return self.__inverted_phonemes[text[i:i + 2]]
         elif (i > 0 or elems[-1].isalpha()) and text[i:i + 2].lower() in self.IOT_VOWELS:
-                return self.inverted_phonemes[text[i:i + 2]]
-        elif i > 0 and not self._is_vowel(elems[-1]) and self._is_solid_sign_possible(text, i):
-            return 'ъ' + self.inverted_phonemes[text[i:i + 2]]
+                return self.__inverted_phonemes[text[i:i + 2]]
+        elif i > 0 and not self.__is_vowel(elems[-1]) and self.__is_solid_sign_possible(text, i):
+            return 'ъ' + self.__inverted_phonemes[text[i:i + 2]]
         else:
             return answer
 
-    def _transliterate_ch_sequence(self, text, i):
+    def __transliterate_ch_sequence(self, text, i):
         if i + 4 < len(text) and text[i + 2:i + 4] in self.CH_AFTER_SEQ:
             letter = self.CH_AFTER_SEQ[text[i + 2:i + 4]]
             return letter.upper() if text[i:i + 2].istitle() else letter
         else:
-            return self.inverted_phonemes[text[i:i + 2]]
+            return self.__inverted_phonemes[text[i:i + 2]]
 
-    def _transliterate_ch_end_sequence(self, text, elems, i, is_upper):
+    def __transliterate_ch_end_sequence(self, text, elems, i, is_upper):
         if ''.join(elems[-2:]) in ['ви', 'ны'] and is_upper == 1:
-            return self.inverted_phonemes[text[i:i + 2]]
+            return self.__inverted_phonemes[text[i:i + 2]]
         elif elems[-1] in 'иы':
             return 'х'
         else:
             return 'ч'
 
-    def _transliterate_vowel_ending(self, text, elems, i):
-        return 'ий' if re.search(r'[гджкцчшщ]$', elems[-1]) else self.inverted_phonemes[text[i:i + 2]]
+    def __transliterate_vowel_ending(self, text, elems, i):
+        return 'ий' if re.search(r'[гджкцчшщ]$', elems[-1]) else self.__inverted_phonemes[text[i:i + 2]]
 
     def transliterate_two_letter_sequence(self, text, elems, i, is_upper):
-        if self.CH_START_REGEX.search(text[:i + 2]):
-            res = self._transliterate_ch_sequence(text, i)
-        elif self.CH_END_REGEX.search(text[i:]):
-            res = self._transliterate_ch_end_sequence(text, elems, i, is_upper)
-        elif self._is_iot_combination(text[i:i + 2]):
-            answer = self.COMBINATED_PHONEMES[text[i:i + 2]] if text[i:i + 2].lower() in self.IET_VOWELS \
-                else self.COMBINATED_E_PHONEMES[text[i:i + 2]] if text[i:i + 2].lower() in self.IOT_VOWELS \
-                else self.inverted_phonemes[text[i:i + 2]]
-            res = self._transliterate_vowels_sequence(text, answer, elems, i)
+        if self.__CH_START_REGEX.search(text[:i + 2]):
+            res = self.__transliterate_ch_sequence(text, i)
+        elif self.__CH_END_REGEX.search(text[i:]):
+            res = self.__transliterate_ch_end_sequence(text, elems, i, is_upper)
+        elif self.__is_iot_combination(text[i:i + 2]):
+            answer = self.__COMBINATED_PHONEMES[text[i:i + 2]] if text[i:i + 2].lower() in self.IET_VOWELS \
+                else self.__COMBINATED_E_PHONEMES[text[i:i + 2]] if text[i:i + 2].lower() in self.IOT_VOWELS \
+                else self.__inverted_phonemes[text[i:i + 2]]
+            res = self.__transliterate_vowels_sequence(text, answer, elems, i)
         elif text[i:i + 2].lower() in ['ij', 'iy', 'yi', 'yj'] and not text[i + 2].isalnum():
-            res = self._transliterate_vowel_ending(text, elems, i)
+            res = self.__transliterate_vowel_ending(text, elems, i)
         elif text[i:i + 2].lower() in ['ij', 'iy', 'yi', 'yj']:
-            res = self.inverted_phonemes[text[i:i + 1]]
+            res = self.__inverted_phonemes[text[i:i + 1]]
             i -= 1
         else:
-            res = self.inverted_phonemes[text[i:i + 2]]
+            res = self.__inverted_phonemes[text[i:i + 2]]
         return res, i
 
     def simple_spell_euristic(self, word):
-        if not self._is_shch_exceptions(word):
-            for phoneme, replaced in self.UNCORRECTED_PHONEMES.items():
+        if not self.__is_shch_exceptions(word):
+            for phoneme, replaced in self.__UNCORRECTED_PHONEMES.items():
                 word = phoneme.sub(replaced, word, re.IGNORECASE)
         return word
 
     def transliterate(self, text):
-        return text.translate(self.keys)
+        return text.translate(self.__keys)
 
     def inverse_transliterate(self, text):
         elems = []
@@ -240,35 +240,35 @@ class Transliterator:
                 is_upper = 1 if text[i].isupper() else 0
 
             if i == len(text) - 1 or text[i + 1] in string.punctuation or text[i + 1].isspace():
-                elems += [self._transliterate_ij_ending(text, elems, i)]
+                elems += [self.__transliterate_ij_ending(text, elems, i)]
                 i += 1
-            elif i + 4 <= len(text) and text[i:i + 4] in self.inverted_phonemes:
-                phoneme = self.inverted_phonemes[text[i:i + 4]]
+            elif i + 4 <= len(text) and text[i:i + 4] in self.__inverted_phonemes:
+                phoneme = self.__inverted_phonemes[text[i:i + 4]]
                 elems += [phoneme] if isinstance(phoneme, str) else phoneme
                 i += 4
-            elif i + 3 <= len(text) and text[i:i + 3] in self.inverted_phonemes:
-                phoneme = self.inverted_phonemes[text[i:i + 3]]
+            elif i + 3 <= len(text) and text[i:i + 3] in self.__inverted_phonemes:
+                phoneme = self.__inverted_phonemes[text[i:i + 3]]
                 elems += [phoneme] if isinstance(phoneme, str) else phoneme
                 i += 3
-            elif i + 2 <= len(text) and text[i:i + 2] in self.inverted_phonemes:
+            elif i + 2 <= len(text) and text[i:i + 2] in self.__inverted_phonemes:
                 answer, i = self.transliterate_two_letter_sequence(text, elems, i, is_upper)
                 elems += answer
                 i += 2
             else:
-                output_symbol = self.inverted_phonemes[text[i]] if text[i] in self.inverted_phonemes else text[i]
+                output_symbol = self.__inverted_phonemes[text[i]] if text[i] in self.__inverted_phonemes else text[i]
                 elems += [output_symbol]
                 i += 1
 
         res = ''.join(elems)
         res = self.simple_spell_euristic(res)
 
-        if self.need_spell:
+        if self.__need_spell:
             pass
             # todo: add spell checker for this case
             '''
             elements = [
-                self.spell_checker.rectify(token.Value) if token.Type == 'WORD' else token.Value
-                for token in self.tokenizer.tokenize(res)
+                self.__spell_checker.rectify(token.Value) if token.Type == 'WORD' else token.Value
+                for token in self.__tokenizer.tokenize(res)
             ]
             res = ' '.join(elements)
             '''
