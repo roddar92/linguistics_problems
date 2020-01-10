047de0a9d61652335863abe05ed377f9d411e256
Dashka Rodionova <rodionowadarja@yandex.ru>
2020-01-10 15:08:30 +0300

    MaskSearch --- Great code refactoring
    Covering of for ? and * cases

--- src/russian/MaskSearch.py
+++ src/russian/MaskSearch.py
@@ -15,7 +15,7 @@ class RegexTrie:
         self.__dfs(self.children, key, candidates=answer, start=0, prefix="")
         return answer
 
-    def __dfs(self, node, key, candidates, start, prefix):
+    def __dfs_light(self, node, key, candidates, start, prefix):
         if start == len(key) and 'is_leaf' in node:
             candidates.append(prefix)
             return
@@ -26,14 +26,47 @@ class RegexTrie:
         elif ch == '?':
             for letter in node:
                 self.__dfs(node[letter], key, candidates, start=start + 1, prefix=prefix + letter)
+                
+    def __dfs(self, node, key, candidates, start, prefix, next_ch=''):
+        if 'is_leaf' in node and len(node) == 1 and next_ch not in ('', '*'):
+            return
+
+        if start == len(key) and 'is_leaf' in node:
+            candidates.append(prefix)
+            return
+
+        ch = key[start] if start < len(key) else '*'
+        if ch in node:
+            self.__dfs(node[ch], key, candidates, start=start + 1, prefix=prefix + ch)
+        elif ch == '?':
+            for letter in node:
+                self.__dfs(node[letter], key, candidates, start=start + 1, prefix=prefix + letter)
+        elif ch == '*':
+            next_ch = key[start + 1] if start + 1 < len(key) else '*'
+
+            if next_ch in node:
+                self.__dfs(node[next_ch], key, candidates, start=start + 2, prefix=prefix + next_ch)
+
+            for letter in node:
+                if letter == 'is_leaf' and len(node) > 1:
+                    continue
+
+                if 'is_leaf' in node and next_ch == '*':
+                    candidates.append(prefix)
+
+                if letter != 'is_leaf':
+                    self.__dfs(node[letter], key, candidates, start=start, prefix=prefix + letter, next_ch=next_ch) 
 
 
 if __name__ == '__main__':
-    VOCABULARY = ['apple', 'orange', 'tomato', 'apfle', 'sandwich', 'avocado', 'tonic', 'timato']
+    VOCABULARY = ['ale', 'apple', 'orange', 'tomato', 'apfle', 'sandwich', 'avocado', 'tonic', 'timato']
     trie = RegexTrie()
     for w in VOCABULARY:
         trie.add(w)
+        
+    assert trie.search_all('a*le') == ['ale', 'apple', 'apfle']
     assert trie.search_all('t?mato') == ['tomato', 'timato']
+    assert trie.search_all('t?mato*') == ['tomato', 'tomatosoup', 'timato']
     assert trie.search_all('ap?le') == ['apple', 'apfle']
     assert trie.search_all('a??le') == ['apple', 'apfle']
     assert trie.search_all('orange') == ['orange']
