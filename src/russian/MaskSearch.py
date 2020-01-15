93485140e89138924fe5f8004a88552bae16f839
roddar92 <rodionowadarja@yandex.ru>
2020-01-15 20:22:06 +0300

    MaskSearch -- Minor code refactoring

--- src/russian/MaskSearch.py
+++ src/russian/MaskSearch.py
@@ -50,14 +50,11 @@ class RegexTrie:
                 self.__dfs(node[next_ch], key, candidates, start=start + 2, prefix=prefix + next_ch)
 
             for letter in node:
-                if letter == 'is_leaf' and len(node) > 1:
-                    continue
-
-                if 'is_leaf' in node and next_ch == '*':
+                if letter == 'is_leaf' and next_ch == '*':
                     candidates.append(prefix)
 
                 if letter != 'is_leaf':
-                    self.__dfs(node[letter], key, candidates, start=start, prefix=prefix + letter, next_ch=next_ch) 
+                    self.__dfs(node[letter], key, candidates, start=start, prefix=prefix + letter, next_ch=next_ch)
 
 
 if __name__ == '__main__':
@@ -67,9 +64,9 @@ if __name__ == '__main__':
     for w in VOCABULARY:
         trie.add(w)
         
-    assert trie.search_all('*') == sorted(VOCABULARY)
-    assert trie.search_all('?')) == []
-    assert trie.search_all('???')) == ['ale']
+    assert sorted(trie.search_all('*')) == sorted(VOCABULARY)
+    assert trie.search_all('?') == []
+    assert trie.search_all('???') == ['ale']
     assert trie.search_all('a*le') == ['ale', 'apple', 'apfle', 'avocadole']
     assert trie.search_all('t?mato') == ['tomato', 'timato']
     assert trie.search_all('t?mato*') == ['tomato', 'tomatosoup', 'timato']
