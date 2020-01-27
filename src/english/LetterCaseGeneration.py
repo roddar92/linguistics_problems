ec3c3f2ad9a572105b9bcb75d71b317a0ec368a6
roddar92 <rodionowadarja@yandex.ru>
2020-01-27 22:34:13 +0300

    LetterCaseGenerator -- Minor code refactoring

--- src/english/LetterCaseGeneration.py
+++ src/english/LetterCaseGeneration.py
@@ -1,18 +1,21 @@
 class LetterCaseGenerator:
-    def generate_strings_in_any_case(self, text: str):
+    @staticmethod
+    def generate_strings_in_any_case(text: str):
             def generate(prefix, i):
                 if i == len(text): 
                     variants.append(prefix)
                     return
 
-                l = text[i]
-                if l in '0123456789':
-                    generate(prefix + l, i + 1)
+                letter = text[i]
+                if letter in '0123456789':
+                    generate(prefix + letter, i + 1)
                 else:
-                    generate(prefix + l.lower(), i + 1)
-                    generate(prefix + l.upper(), i + 1)
+                    generate(prefix + letter.lower(), i + 1)
+                    generate(prefix + letter.upper(), i + 1)
 
-            if not text: return text
+            if not text:
+                return text
+
             variants = []
             generate('', 0)
             return variants
@@ -22,4 +25,4 @@ if __name__ == '__main__':
     lcg = LetterCaseGenerator()
     assert lcg.generate_strings_in_any_case('a1b2') == ['a1b2', 'a1B2', 'A1b2', 'A1B2']
     assert lcg.generate_strings_in_any_case('3z4') == ['3z4', '3Z4']
-    assert lcg.generate_strings_in_any_case('3z4') == ['123', '123']
+    assert lcg.generate_strings_in_any_case('123') == ['123']
