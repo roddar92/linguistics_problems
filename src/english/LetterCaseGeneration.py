d12a90787d30b161b5c2bb5d1e26934104b0a91e
roddar92 <rodionowadarja@yandex.ru>
2020-03-23 21:50:33 +0300

    LetterCaseGenerator -- Add another generation method

--- src/english/LetterCaseGeneration.py
+++ src/english/LetterCaseGeneration.py
@@ -1,24 +1,46 @@
 class LetterCaseGenerator:
     @staticmethod
     def generate_strings_in_any_case(text: str):
-            def generate(prefix, i):
-                if i == len(text): 
-                    variants.append(prefix)
-                    return
+        def generate(prefix, i):
+            if i == len(text):
+                variants.append(prefix)
+                return
 
-                letter = text[i]
-                if letter in '0123456789':
-                    generate(prefix + letter, i + 1)
+            letter = text[i]
+            if letter.isdigit():
+                generate(prefix + letter, i + 1)
+            else:
+                generate(prefix + letter.lower(), i + 1)
+                generate(prefix + letter.upper(), i + 1)
+
+        if not text:
+            return text
+
+        variants = []
+        generate('', 0)
+        return variants
+
+    @staticmethod
+    def generate_strings_in_any_case_2(text: str):
+        if not text:
+            return text
+
+        variants, stack = set(), [('', 0)]
+        while stack:
+            prefix, ind = stack.pop()
+            if ind == len(text):
+                variants.add(prefix)
+
+            if ind < len(text):
+                letter = text[ind]
+
+                if letter.isdigit():
+                    stack.append((prefix + letter, ind + 1))
                 else:
-                    generate(prefix + letter.lower(), i + 1)
-                    generate(prefix + letter.upper(), i + 1)
+                    stack.append((prefix + letter.lower(), ind + 1))
+                    stack.append((prefix + letter.upper(), ind + 1))
 
-            if not text:
-                return text
-
-            variants = []
-            generate('', 0)
-            return variants
+        return variants
             
         
 if __name__ == '__main__':
@@ -26,3 +48,7 @@ if __name__ == '__main__':
     assert lcg.generate_strings_in_any_case('a1b2') == ['a1b2', 'a1B2', 'A1b2', 'A1B2']
     assert lcg.generate_strings_in_any_case('3z4') == ['3z4', '3Z4']
     assert lcg.generate_strings_in_any_case('123') == ['123']
+
+    assert lcg.generate_strings_in_any_case_2('a1b2') == {'a1b2', 'a1B2', 'A1b2', 'A1B2'}
+    assert lcg.generate_strings_in_any_case_2('3z4') == {'3z4', '3Z4'}
+    assert lcg.generate_strings_in_any_case_2('123') == {'123'}
