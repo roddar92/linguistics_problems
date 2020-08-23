a77ace93f8d4a4012f2a18e2dbd54c1aca09b379
roddar92 <rodionowadarja@yandex.ru>
2020-08-24 01:08:57 +0300

    StringsGenerator --- Add a bit of statistics

--- src/english/StringsGenerator.py
+++ src/english/StringsGenerator.py
@@ -1,5 +1,5 @@
 from random import choice, choices
-from string import ascii_uppercase
+from string import ascii_uppercase, ascii_lowercase
 
 
 class StringGenerator:
@@ -15,8 +15,8 @@ class StringGenerator:
             while stack:
                 alphabet, prefix, gen_prob = stack.pop()
                 stop_prob = 1 - gen_prob
-                rand_num = choices(range(2), [stop_prob, gen_prob])
-                if rand_num[0] == 0:
+                rand_num = choices(range(2), [stop_prob, gen_prob])[-1]
+                if rand_num == 0:
                     result.add(''.join(prefix))
                 else:
                     next_letter = choice(alphabet)
@@ -31,9 +31,10 @@ class StringGenerator:
 
 
 if __name__ == '__main__':
-    data = StringGenerator.generate_string_list(list(ascii_uppercase))
-    multi_letter_subset_len = len(list(filter(lambda x: x > 1, map(len, data))))
+    data = StringGenerator.generate_string_list(list(ascii_uppercase + ascii_lowercase))
+    multi_letter_subset = list(filter(lambda x: x > 1, map(len, data)))
     print('Set:', data)
     print('Length:', len(data))
-    print('Count of multi-chars:', multi_letter_subset_len)
-    print('Count of chars:', len(data) - multi_letter_subset_len)
+    print('Count of multi-chars:', len(multi_letter_subset))
+    print('Count of chars:', len(data) - len(multi_letter_subset))
+    print('Max string len:', max(multi_letter_subset))
