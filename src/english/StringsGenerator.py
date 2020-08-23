758d4ed41284a06712d6c315a29431de17a6ddfd
roddar92 <rodionowadarja@yandex.ru>
2020-08-23 15:02:26 +0300

    StringsGenerator --- Create simple generator of strings from any set

---
+++ src/english/StringsGenerator.py
@@ -0,0 +1,40 @@
+from random import choice, choices
+from string import ascii_uppercase
+
+
+class StringGenerator:
+    @staticmethod
+    def generate_string_list(strings, generation_prob=.75):
+        result = set()
+
+        global_alphabet = strings
+        for letter in global_alphabet:
+            global_alphabet.remove(letter)
+            stack = [
+                (global_alphabet, [letter], generation_prob)
+            ]
+            while stack:
+                alphabet, prefix, gen_prob = stack.pop()
+                stop_prob = 1 - gen_prob
+                rand_num = choices(range(2), [stop_prob, gen_prob])
+                if rand_num[0] == 0:
+                    result.add(''.join(prefix))
+                else:
+                    next_letter = choice(alphabet)
+                    alphabet.remove(next_letter)
+                    stack.append(
+                        (alphabet, prefix + [next_letter], gen_prob ** 2)
+                    )
+                    alphabet.append(next_letter)
+
+            global_alphabet.append(letter)
+        return result
+
+
+if __name__ == '__main__':
+    data = StringGenerator.generate_string_list(list(ascii_uppercase))
+    multi_letter_subset_len = len(list(filter(lambda x: x > 1, map(len, data))))
+    print('Set:', data)
+    print('Length:', len(data))
+    print('Count of multi-chars:', multi_letter_subset_len)
+    print('Count of chars:', len(data) - multi_letter_subset_len)
