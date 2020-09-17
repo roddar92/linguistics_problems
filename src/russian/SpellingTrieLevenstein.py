ece7e4559c150a6031faf19a0b5b7440df23609e
roddar92 <rodionowadarja@yandex.ru>
2020-09-18 00:22:42 +0300

    SpellingLevensteinTrie --- Code refactoring

--- src/russian/SpellingTrieLevenstein.py
+++ src/russian/SpellingTrieLevenstein.py
@@ -1,4 +1,5 @@
-from typing import Dict, List, Tuple
+from sortedcontainers import SortedDict, SortedListWithKey
+from typing import Dict, List
 
 
 class SpellingLevensteinTree:
@@ -15,7 +16,7 @@ class SpellingLevensteinTree:
         node = self.root
         for l in word:
             if l not in node:
-                node[l] = {}
+                node[l] = SortedDict()
             node = node[l]
         node['is_leaf'] = True
 
@@ -27,13 +28,13 @@ class SpellingLevensteinTree:
             self.add(word)
 
     @staticmethod
-    def __find_best_candidates(word: str, dist: int, candidates: List[Tuple[str, int]],
+    def __find_best_candidates(word: str, dist: int, candidates: SortedListWithKey,
                                node: Dict[str, Dict], prefix: List[str], last_row: List[int]) -> None:
         def __dfs(curr_node, curr_prefix, prev_row):
             current_row = [prev_row[0] + 1]
             min_dist = current_row[0]
 
-            for i in range(1, chars):
+            for i in range(1, len(word) + 1):
                 insert_or_del = min(current_row[i - 1], prev_row[i]) + 1
                 replace = prev_row[i - 1] + int(word[i - 1] != curr_prefix[-1])
                 current_row.append(min(insert_or_del, replace))
@@ -43,13 +44,12 @@ class SpellingLevensteinTree:
                 return
 
             if current_row[-1] <= dist and 'is_leaf' in curr_node:
-                candidates.append((''.join(curr_prefix), current_row[-1]))
+                candidates.add((''.join(curr_prefix), current_row[-1]))
 
             for ll in curr_node:
                 if ll != 'is_leaf':
                     __dfs(curr_node[ll], curr_prefix + [ll], current_row)
 
-        chars = len(word) + 1
         __dfs(node, prefix, last_row)
 
     def find_longest_prefix(self, word: str) -> str:
@@ -64,11 +64,11 @@ class SpellingLevensteinTree:
             node = node[letter]
         return word[:pos] if pos >= 0 else word
 
-    def search(self, word: str, distance=0) -> List[Tuple[str, int]]:
+    def search(self, word: str, distance=0) -> SortedListWithKey:
         """
         Returns candidates list with words that equal to the given word after its modifying with Levenstein distance
         """
-        candidates = []
+        candidates = SortedListWithKey(key=lambda x: x[-1])
         current_row = list(range(len(word) + 1))
         for letter in self.root:
             self.__find_best_candidates(word, distance, candidates, self.root[letter], [letter], current_row)
@@ -80,15 +80,15 @@ if __name__ == '__main__':
     dictionary.build_dict(['hello', 'hallo', 'leetcode', 'hell'])
 
     assert dictionary.search('hello') == [('hello', 0)]
-    assert dictionary.search('hhllo', 1) == [('hello', 1), ('hallo', 1)]
-    assert dictionary.search('hhllo', 2) == [('hell', 2), ('hello', 1), ('hallo', 1)]
-    assert dictionary.search('hkelo', 2) == [('hell', 2), ('hello', 2), ('hallo', 2)]
+    assert dictionary.search('hhllo', 1) == [('hallo', 1), ('hello', 1)]
+    assert dictionary.search('hhllo', 2) == [('hallo', 1), ('hello', 1), ('hell', 2)]
+    assert dictionary.search('hkelo', 2) == [('hallo', 2), ('hell', 2), ('hello', 2)]
     assert not dictionary.search('hklo')
-    assert dictionary.search('hklo', 2) == [('hell', 2), ('hello', 2), ('hallo', 2)]
+    assert dictionary.search('hklo', 2) == [('hallo', 2), ('hell', 2), ('hello', 2)]
     assert dictionary.search('lettcode', 2) == [('leetcode', 1)]
-    assert dictionary.search('hkloo', 2) == [('hello', 2), ('hallo', 2)]
+    assert dictionary.search('hkloo', 2) == [('hallo', 2), ('hello', 2)]
     assert dictionary.search('elloo', 2) == [('hello', 2)]
-    assert dictionary.search('elloo', 3) == [('hell', 3), ('hello', 2), ('hallo', 3)]
+    assert dictionary.search('elloo', 3) == [('hello', 2), ('hallo', 3), ('hell', 3)]
     assert dictionary.find_longest_prefix('elloo') == ''
     assert dictionary.find_longest_prefix('hkloo') == 'h'
     assert dictionary.find_longest_prefix('lettcode') == 'le'
