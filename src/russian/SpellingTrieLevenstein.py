54b51d7b12e9a581e869d33baa6f0a81124eb1e4
roddar92 <rodionowadarja@yandex.ru>
2020-09-18 21:32:36 +0300

    SpellingTrieLevenstein --- Main code moving

--- src/russian/SpellingTrieLevenstein.py
+++ src/russian/SpellingTrieLevenstein.py
@@ -1,5 +1,5 @@
 from sortedcontainers import SortedDict, SortedListWithKey
-from typing import Dict, List
+from typing import List
 
 
 class SpellingLevensteinTree:
@@ -27,31 +27,6 @@ class SpellingLevensteinTree:
         for word in words:
             self.add(word)
 
-    @staticmethod
-    def __find_best_candidates(word: str, dist: int, candidates: SortedListWithKey,
-                               node: Dict[str, Dict], prefix: List[str], last_row: List[int]) -> None:
-        def __dfs(curr_node, curr_prefix, prev_row):
-            current_row = [prev_row[0] + 1]
-            min_dist = current_row[0]
-
-            for i in range(1, len(word) + 1):
-                insert_or_del = min(current_row[i - 1], prev_row[i]) + 1
-                replace = prev_row[i - 1] + int(word[i - 1] != curr_prefix[-1])
-                current_row.append(min(insert_or_del, replace))
-                min_dist = min(min_dist, current_row[-1])
-
-            if min_dist > dist:
-                return
-
-            if current_row[-1] <= dist and 'is_leaf' in curr_node:
-                candidates.add((''.join(curr_prefix), current_row[-1]))
-
-            for ll in curr_node:
-                if ll != 'is_leaf':
-                    __dfs(curr_node[ll], curr_prefix + [ll], current_row)
-
-        __dfs(node, prefix, last_row)
-
     def find_longest_prefix(self, word: str) -> str:
         """
         Find the longest word prefix in a trie dictionary
@@ -68,10 +43,30 @@ class SpellingLevensteinTree:
         """
         Returns candidates list with words that equal to the given word after its modifying with Levenstein distance
         """
+        def __dfs(curr_node, curr_prefix, prev_row):
+            curr_row = [prev_row[0] + 1]
+            min_dist = curr_row[0]
+
+            for i in range(1, len(word) + 1):
+                insert_or_del = min(curr_row[i - 1], prev_row[i]) + 1
+                replace = prev_row[i - 1] + int(word[i - 1] != curr_prefix[-1])
+                curr_row.append(min(insert_or_del, replace))
+                min_dist = min(min_dist, curr_row[-1])
+
+            if min_dist > distance:
+                return
+
+            if curr_row[-1] <= distance and 'is_leaf' in curr_node:
+                candidates.add((''.join(curr_prefix), curr_row[-1]))
+
+            for ll in curr_node:
+                if ll != 'is_leaf':
+                    __dfs(curr_node[ll], curr_prefix + [ll], curr_row)
+
         candidates = SortedListWithKey(key=lambda x: x[-1])
         current_row = list(range(len(word) + 1))
         for letter in self.root:
-            self.__find_best_candidates(word, distance, candidates, self.root[letter], [letter], current_row)
+            __dfs(self.root[letter], [letter], current_row)
         return candidates
 
 
